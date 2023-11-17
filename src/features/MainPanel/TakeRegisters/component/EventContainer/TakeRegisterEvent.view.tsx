2d68f374e4f7ff2631afcb2b025ac62f564c767e
Gulafsha.Ali <Gulafsha.Ali@ESS-22P4SQ2>
2023-11-17 14:58:31 +0530

    #32804 | Gulafsha, Komal | No More register tile

--- src/features/MainPanel/TakeRegisters/component/EventContainer/TakeRegisterEvent.view.tsx
+++ src/features/MainPanel/TakeRegisters/component/EventContainer/TakeRegisterEvent.view.tsx
@@ -136,7 +136,7 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
       </div>
 
       <div className="slider-div">
-        {apiError === false && apiRegsiterEventData && (
+        {apiError === false && (
           <Carousel
             ref={carouselRef}
             slidesToSlide={3}
@@ -186,7 +186,7 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
                 />
               </div>
             )}
-            {/* { apiRegsiterEventData.length > 0 &&
+            { apiRegsiterEventData !=null && apiRegsiterEventData.length > 0 &&
               <div className="actioncard-div">
                 <ActionCard
                   dataTestId="test-id"
@@ -196,7 +196,7 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
                   primaryText="No more registers"
                 />
               </div>
-              } */}
+              }
           </Carousel>
         )}
       </div>
