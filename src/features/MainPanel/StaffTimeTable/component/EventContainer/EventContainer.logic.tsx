09ae40e1f934092d04391f45bf370ed2e11e502f
Annapureddy Bhargavi <annapureddy.bhargavi@parentpay.com>
2023-11-20 09:28:48 +0000

    Merged PR 3597: #35862 |Bhargavi| Added background color
    #35862 |Bhargavi| Added background color
    
    Related work items: #35862

--- src/features/MainPanel/StaffTimeTable/component/EventContainer/EventContainer.logic.tsx
+++ src/features/MainPanel/StaffTimeTable/component/EventContainer/EventContainer.logic.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./style.scss";
-import { EventCard, EventCardStatus } from "@essnextgen/ui-kit";
+import { EventCard } from "@essnextgen/ui-kit";
 import dayjs from "dayjs";
 import { FetchStaffTimeTableEventsData } from "../../../../../shared/services/schoolDomain/schoolServices";
 import { EventContainerView } from "./EventContainer.view";
@@ -70,7 +70,7 @@ const EventContainer: React.FC = () => {
         id="no-events-today-id"
         primaryText=""
         secondaryText=""
-        status={EventCardStatus.DEFAULT}
+       // status={EventCardStatus.DEFAULT}
         title="No events today"
         inputWidth={166}
         inputHeight={75}
@@ -108,7 +108,7 @@ const EventContainer: React.FC = () => {
           id="no-events-to-display-id"
           primaryText=""
           secondaryText=""
-          status={EventCardStatus.DEFAULT}
+          // status={EventCardStatus.DEFAULT}
           title="No events to display"
           inputWidth={166}
           inputHeight={75}
