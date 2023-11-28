a145fe717901b577e1b6069c49d39d12ded2273a
Annapureddy Bhargavi <annapureddy.bhargavi@parentpay.com>
2023-11-28 04:39:54 +0000

    Merged PR 3926: #32446 |Bhargavi,Akash,Gulafsha| updated focus class for Eventcard
    #32446 |Bhargavi,Akash,Gulafsha| updated focus class for Eventcard
    
    Related work items: #32446

--- src/features/MainPanel/StaffTimeTable/component/EventContainer/EventContainer.view.tsx
+++ src/features/MainPanel/StaffTimeTable/component/EventContainer/EventContainer.view.tsx
@@ -39,6 +39,7 @@ export const EventContainerView: (
         title={EventTitle}
         inputWidth={166}
         inputHeight={75}
+        tabIndex={0}
         dataTestId={`eventid${index}`}    
         /* eslint-disable */  
         className={SelectedItem === SchoolEventexternalId ? (index===0?`dynamiceventcard event-primary-text event-highlight-0`:`dynamiceventcard event-primary-text event-${EventCardColor}-1`) : `dynamiceventcard event-primary-text`}
