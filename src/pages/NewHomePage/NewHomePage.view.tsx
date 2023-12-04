cdce22edc9b704d587163a2db3f7ffe94257f29b
Annapureddy Bhargavi <annapureddy.bhargavi@parentpay.com>
2023-12-04 10:49:15 +0000

    Merged PR 4253: "#Nocard |Bhargavi| Layout fixes for quicklink page
    "#Nocard |Bhargavi| Layout fixes for quicklink page
    
    Related work items: #37049

--- src/pages/NewHomePage/NewHomePage.view.tsx
+++ src/pages/NewHomePage/NewHomePage.view.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "react-router-dom";
 import { authService, MatchPermissions, Permission } from "@essnextgen/auth-ui";
 import "./style.scss";
 import { Grid, GridItem } from "@essnextgen/ui-kit";
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { envConfig } from "../../shared/utils";
 import MainPanelView from "../../features/MainPanel/MainPanel.view";
 import SidePanelView from "../../features/SidePanel/SidePanel.view";
@@ -17,74 +17,96 @@ const requiredPermissions: Permission[] = [
   }
 ];
 
-export const NewHomepageView : () => JSX.Element = () => {
+export const NewHomepageView: () => JSX.Element = () => {
   const isPermission =
     authService.isAuthorised(requiredPermissions, MatchPermissions.all) &&
     envConfig.IS_NEWHOMEPAGE_ACCESSIBLE === "True";
-  const [isOpen, setIsOpen]:[boolean,React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(true);
-  const [showQuickLink, setShowQuickLink]:[boolean,React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(false);
+  const [isOpen, setIsOpen]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState<boolean>(true);
+  const [showQuickLink, setShowQuickLink]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState<boolean>(false);
 
- 
-  const [quickLinkData, setQuickLinkData]:[IQuickLinkApiResponse[] | null,React.Dispatch<React.SetStateAction<IQuickLinkApiResponse[] | null>>] = useState<IQuickLinkApiResponse[] | null>(null);
-   const [isError, setIsError]:[boolean,React.Dispatch<React.SetStateAction<boolean>>] = useState<boolean>(false);
-   const showQuickLinkView :()=>void= () => {
+  const [quickLinkData, setQuickLinkData]: [
+    IQuickLinkApiResponse[] | null,
+    React.Dispatch<React.SetStateAction<IQuickLinkApiResponse[] | null>>
+  ] = useState<IQuickLinkApiResponse[] | null>(null);
+  const [isError, setIsError]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>
+  ] = useState<boolean>(false);
+  const showQuickLinkView: () => void = () => {
     setShowQuickLink(true);
   };
 
-  const showMainPanelView:()=>void= () => {
+  const showMainPanelView: () => void = () => {
     setShowQuickLink(false);
   };
-  const togglePanel:()=>void = () => {
+  const togglePanel: () => void = () => {
     setIsOpen(!isOpen);
   };
 
-  const closePanel:()=>void = () => {
+  const closePanel: () => void = () => {
     setIsOpen(false);
   };
 
   useEffect(() => {
     (async () => {
-      try {          
-        const responseapidata: {
-          response: IQuickLinkApiResponse[];
-          status: boolean;
-      } | null | undefined  = await fetchQuickLinkDetails(); 
-       if( responseapidata !=null )
-       { 
-        setQuickLinkData(responseapidata?.response);
-        setIsError(responseapidata.status);
-
-       }
-       
-      } catch (error) { 
-        console.log(error);       
-      }      
+      try {
+        const responseapidata:
+          | {
+              response: IQuickLinkApiResponse[];
+              status: boolean;
+            }
+          | null
+          | undefined = await fetchQuickLinkDetails();
+        if (responseapidata != null) {
+          setQuickLinkData(responseapidata?.response);
+          setIsError(responseapidata.status);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     })();
-  }, []); 
-  
+  }, []);
+
   return isPermission ? (
     <>
       <Grid className="app">
-        <GridItem className="side-margin" lg={isOpen ? 3 : 1}   md = {isOpen ? 2 : 1} sm ={isOpen ? 1 : 0}>
+        <GridItem
+          className={isOpen ? "side-margin" : "side-margin-closed"}
+          lg={isOpen ? 3 : 2}
+          md={isOpen ? 2 : 1}
+          sm={isOpen ? 1 : 0}
+        >
           <SidePanelView
             isOpen={isOpen}
             togglePanel={togglePanel}
             closePanel={closePanel}
             showQuickLinkView={showQuickLinkView}
             showMainPanelView={showMainPanelView}
-            setQuickLinkData = {setQuickLinkData}
-            quicklinkData = {isError?[]:quickLinkData}
+            setQuickLinkData={setQuickLinkData}
+            quicklinkData={isError ? [] : quickLinkData}
             data-testid="btn-show-quick-link"
           />
         </GridItem>
         <GridItem
           className="body-panel"
-          lg={isOpen ? 9 : 11}
-          md = {isOpen ? 6 : 7}
-          sm = {isOpen ? 3 : 4}
+          lg={isOpen ? 9 : 10}
+          md={isOpen ? 6 : 7}
+          sm={isOpen ? 3 : 4}
         >
-          {showQuickLink ? <QuickLinkLogic  setQuickLinkData = {setQuickLinkData}  apiQuickLinkData={isError?[]:quickLinkData}/> : <MainPanelView />}
-          
+          {showQuickLink ? (
+            <QuickLinkLogic
+              setQuickLinkData={setQuickLinkData}
+              apiQuickLinkData={isError ? [] : quickLinkData}
+            />
+          ) : (
+            <MainPanelView />
+          )}
         </GridItem>
       </Grid>
     </>
