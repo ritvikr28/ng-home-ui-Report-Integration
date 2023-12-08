c1062cd9990c7a6b7272d0bad51d7c3ca91048b9
Annapureddy Bhargavi <annapureddy.bhargavi@parentpay.com>
2023-12-08 17:30:50 +0530

    #37846 |Bhargavi| test case for switch view

---
+++ src/features/MainPanel/SwitchView/__tests__/SwitchView.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import SwitchView from "../SwitchView.view";
+ 
+ 
+describe("SwitchView Component", () => {
+  test("renders correctly when isApiError is false", () => {
+    const mockProps = {
+      organisationName: "My School",
+      isApiError: false,
+    };
+ 
+    render(<SwitchView {...mockProps} />);
+ 
+ 
+    const expectedText = `You are viewing ${mockProps.organisationName} as a Teacher.`;
+    expect(screen.getByText(expectedText)).toBeInTheDocument();
+ 
+ 
+    expect(screen.getByText("Switch view here")).toBeInTheDocument();
+  });
+ 
+  test("renders correctly when isApiError is true", () => {
+    const mockProps = {
+      organisationName: "My School",
+      isApiError: true,
+    };
+ 
+    render(<SwitchView {...mockProps} />);
+ 
+ 
+    const expectedTextRegex = new RegExp(`You are viewing\\s+as a Teacher\\.`);
+    expect(screen.getByText(expectedTextRegex)).toBeInTheDocument();
+ 
+ 
+    expect(screen.getByText("Switch view here")).toBeInTheDocument();
+  });
+ 
+});
