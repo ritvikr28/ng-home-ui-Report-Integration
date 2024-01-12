b029b53d8bce4b7f497f2a3f14e45fc5a2534048
Amey Kolse <Amey.Kolse@parentpay.com>
2024-01-12 19:10:00 +0530

    #40596 | Amey,Gulafsha | SLTMockpage files

--- src/features/MainPanel/SwitchView/SwitchView.view.tsx
+++ src/features/MainPanel/SwitchView/SwitchView.view.tsx
@@ -1,21 +1,36 @@
+import React from "react";
 import { Grid, GridItem } from "@essnextgen/ui-kit";
 import "./style.scss";
 import { ISwitchViewProps } from "./SwitchView.props";
-
+ 
 const SwitchView: (props: ISwitchViewProps) => JSX.Element = (
   props: ISwitchViewProps
 ) => {
-  const { organisationName, isApiError }: ISwitchViewProps = props;
-
+  const { organisationName, isApiError,path }: ISwitchViewProps = props;
+  const handleSwitchViewClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();  
+    if(path === "/"){
+    window.location.href ="/"
+    }
+    else{
+    window.location.href = "/SLTView"
+    }
+  };
+ 
   return (
-    <Grid>      
+    <Grid>
       <GridItem lg={10}>
-      <div>
-          You are viewing {isApiError === false ? organisationName : ""} as a Teacher.
-          </div>
+        <div>
+          You are viewing {isApiError === false ? organisationName : ""} as a
+          Teacher. {/* eslint-disable-next-line */}
+          <a className="link-color" href=" " onClick={handleSwitchViewClick}>
+            Switch view here
+          </a>
+          {/* eslint-enable-next-line */}
+        </div>
       </GridItem>
     </Grid>
   );
 };
-
+ 
 export default SwitchView;
