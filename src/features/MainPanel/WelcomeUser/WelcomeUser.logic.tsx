b445698c5290ccdd6d1a72ed10ba327a692725ff
Namrata Chaudhar <Namrata.Chaudhar@parentpay.com>
2024-01-17 19:58:30 +0530

    [#38605, #38603, #38643, #38644][Namrata | Bhargavi]: Responsiveness bug fixes

--- src/features/MainPanel/WelcomeUser/WelcomeUser.logic.tsx
+++ src/features/MainPanel/WelcomeUser/WelcomeUser.logic.tsx
@@ -1,4 +1,5 @@
 import { authService } from "@essnextgen/auth-ui";
+import { useMediaQuery } from "@essnextgen/ui-kit";
 import React,{ useEffect, useState } from "react";
 import WelcomeUserView from "./WelcomeUser.view";
 import { IWelcomeUserLogicProps } from "./WelcomeUserProps";
@@ -33,12 +34,15 @@ const WelcomeUser:(props: IWelcomeUserLogicProps) => JSX.Element = (
       isMounted = false;
     };
   }, []);
-
+  const isMobileView : boolean = useMediaQuery(
+    "(min-width:320px) and (max-width: 767.9px)"
+  );
 
   const userName:string = (userFullname===null)?"":userFullname.split(" ")[0];
   const isLong:boolean = userName.length > 25;
-  const parentClass:string = isLong ? "parent2" : "parent1";
-  const subparentClass = `${parentClass}-subparent` as string;
+  const parentClass:string =  isLong ?"parent2" :"parent1";
+  const mobileparentClass  = isOpen? "parent1-open": "parent1";
+  const subparentClass = `${isMobileView ? mobileparentClass: parentClass}-subparent` as string;
 
   return (
     <>
