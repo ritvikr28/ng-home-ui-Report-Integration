1be576ff23de2d81e91c2f2c5df3030dea338c06
Namrata Chaudhar <Namrata.Chaudhar@parentpay.com>
2024-01-19 13:17:20 +0530

    [#38605, #38603, #38643, #38644][Namrata | Bhargavi]: Responsiveness bug fixes

--- src/features/MainPanel/WelcomeUser/WelcomeUser.view.tsx
+++ src/features/MainPanel/WelcomeUser/WelcomeUser.view.tsx
@@ -15,7 +15,7 @@ const WelcomeUserView: (props: IWelcomeUserViewProps) => JSX.Element = (
     isOpen
   }: IWelcomeUserViewProps = props;
   const isMobileView : boolean = useMediaQuery(
-    "(min-width:320px) and (max-width: 767.9px)"
+    "(min-width:320px) and (max-width: 1023.9px)"
   );
   return (
     <Grid className={`welcome-parent ${parentClassName}`}>
