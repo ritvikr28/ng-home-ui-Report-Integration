82688f26a8e358a585b6303814cc9a3e74d48630
Amey Kolse <Amey.Kolse@parentpay.com>
2024-02-05 12:35:28 +0530

    #42242 | Amey,Venkatesh | Add workspace id for Ng-Home-Ui

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-window.REACT_API_URL = "http://localhost:5010/api";
+window.REACT_API_URL = "https://api-dev.home.sims.co.uk/api";
 window.REACT_CLIENT_ID = "pm-sso-5f9e0af8-350b-4f53-a722-6585b4806d7f";
 window.REACT_AUTH_ENDPOINT =
   "https://simsid-partner-stsserver.azurewebsites.net/connect";
@@ -6,9 +6,10 @@ window.REACT_REDIRECT_URI = "http://localhost:3000/auth";
 window.REACT_ENVIRONMENT = "localhost";
 window.REACT_GA_TRACKING_ID = "GTM-5LBSRF3";
 window.AppInsightsConnectionString = "";
-window.AUTH_API_URL = "http://localhost:5013/api";
+window.AUTH_API_URL = "http://localhost:5010/api";
 window.APPLICATION = "Home";
 window.IS_NEWHOMEPAGE_ACCESSIBLE = "True";
 window.SCHOOL_BASE_URL="";
 window.REGISTER_BASE_URL="";
 window.LEARNER_UI_URL="";
+window.LOOPEDIN_WORKSPACE_ID = "ef8322c9-2fc1-4082-8722-6e2f07ed4c4c";
