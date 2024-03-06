859f0c9bf616ee4a50d53b90f050cbe6d0e117b0
Gulafsha.Ali <Gulafsha.Ali@ESS-22P4SQ2>
2024-03-06 09:29:04 +0530

    #NoCard | Gulafsha | Managed Other routes

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -216,7 +216,7 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
           <ProtectedRoute exact path="/noAccess" component={NoAccess} />
           {shouldRenderSLTView && <ProtectedRoute exact path="/slt-view" component={SLTmockpage} />}
           {isStandaloneApp && <Route exact path="/auth" component={Auth} />}
-          {isStandaloneApp && <Route exact path="*" component={PageNotFound} />}
+          <Route exact path="*" component={PageNotFound} />
         </Switch>
       </Suspense>
     </Router>
