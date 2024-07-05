9607b17f58e8da40740c03c8e0986b89e2ba922c
Amey Kolse <Amey.Kolse@parentpay.com>
2024-07-05 11:21:52 +0530

    62795 | Amey,Akash | Refactor and add test cases

--- src/features/AdminConsole/AdminConsole.view.tsx
+++ src/features/AdminConsole/AdminConsole.view.tsx
@@ -1,60 +1,64 @@
-import { Breadcrumbs, Grid, GridItem, useMediaQuery } from "@essnextgen/ui-kit";
- import { useState } from "react";
+import {
+  Breadcrumbs,
+  Grid,
+  GridItem,
+  useMediaQuery
+} from "@essnextgen/ui-kit";
+import { useState } from "react";
 import { LocalisedMenu } from "@essnextgen/ui-application-kit";
 import About from "./About";
 
 export interface IAdminConsoleProps {
   baseRouteName: string;
+  togglePanel: any;
 }
 
 const AdminConsole = () => {
   const isMobileView: boolean = useMediaQuery(
     "(min-width:320px) and (max-width: 1023.9px)"
   );
-
+  
   const [isOpen, setIsOpen] = useState<boolean>(!isMobileView);
 
   return (
     <>
       <Grid>
         <GridItem>
-            <LocalisedMenu
-              menuHeading="Admin Console"
-              onCloseSideNavigationPanel={() => setIsOpen(false)}
-              isOpenSideNavigation={isOpen}
-              onMenuItemClick={(e, item) => console.log(item)}
-              defaultSelectedMenu={{
-                text: "Conduct",
-                value: "https://dev.cdn.sims.co.uk/settings",
-              }}              
-            />
+          <LocalisedMenu
+            // customHeight={80}
+            menuHeading="Admin Console"
+            onCloseSideNavigationPanel={() => setIsOpen(false)}
+            isOpenSideNavigation={isOpen}
+            defaultSelectedMenu={{
+              text: "About",
+              value: "/adminconsole",
+            }}
+          />
         </GridItem>
         <GridItem style={{ marginTop: "24px" }}>
-        <div>
-          <Breadcrumbs
-            breadcrumbActions={[
-              {
-                active: true,
-                linkName: "Home",
-                path: window.location.origin,
-              },
-              {
-                active: false,
-                linkName: "Admin Console",
-                path: "#",
-              }
-            ]}
-            className="essui-Breadcrumbs "
-            dataTestId="breadcrumb-test-id"
-            id="element-id"
-            onItemClick={() => {}}
-          />
-          </div>
           <div>
-          <About/>
+            <Breadcrumbs
+              breadcrumbActions={[
+                {
+                  active: true,
+                  linkName: "Home",
+                  path: window.location.origin,
+                },
+                {
+                  active: false,
+                  linkName: "Admin Console",
+                  path: "#",
+                }
+              ]}
+              className="essui-Breadcrumbs"
+              dataTestId="breadcrumb-test-id"
+              id="element-id"
+              onItemClick={() => {}}
+            />
+            <About />
           </div>
         </GridItem>
-       </Grid>
+      </Grid>
     </>
   );
 };
---
+++ src/features/AdminConsole/__tests__/About.spec.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import About from "../About";
+
+describe("About component", () => {
+  it("renders heading and subheading text", () => {
+    render(<About />);
+
+    expect(screen.getByText("About the Admin console")).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "Manage all the MIS background processes and modules' settings centrally from the Admin console."
+      )
+    ).toBeInTheDocument();
+  });
+
+  it("renders HeadingSubHeading component with correct props", () => {
+    render(<About />);
+
+    const headingElement = screen.getByText("About the Admin console");
+    const subHeadingElement = screen.getByText(
+      "Manage all the MIS background processes and modules' settings centrally from the Admin console."
+    );
+
+    expect(headingElement).toBeInTheDocument();
+    expect(subHeadingElement).toBeInTheDocument();
+  });
+  it("renders HeadingSubHeading component with correct props", () => {
+    render(<About />);
+
+    const textElements = screen.getAllByText(
+      /About the Admin console|Manage all the MIS background processes and modules settings centrally from the Admin console./
+    );
+
+    expect(textElements).toHaveLength(1);
+  });
+});
