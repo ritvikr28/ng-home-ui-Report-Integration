8cda05e18d04ed74b21d95d8a387be44f2b64f84
Amey Kolse <Amey.Kolse@parentpay.com>
2024-07-15 11:13:39 +0530

    #62987 | Amey,Bhargavi | Alignment fixes for Admin Console

--- src/features/AdminConsole/About.tsx
+++ src/features/AdminConsole/About.tsx
@@ -2,7 +2,7 @@ import { HeadingSubHeading } from "@essnextgen/ui-kit";
 import "./style.scss";
 
 const About: () => JSX.Element = () => (
-  <div className="admin-heading sub-text-131">
+  <div className="admin-heading heading-text-up">
     <HeadingSubHeading
       headingText="About the Admin console"
       subHeadingText="Manage all the MIS background processes and modules settings centrally from the Admin console."
