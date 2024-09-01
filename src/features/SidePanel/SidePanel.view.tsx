48ec5340fd506b0cfd6cc7cd270ac97dc5df640e
Amey Kolse <Amey.Kolse@parentpay.com>
2024-09-01 14:41:33 +0530

    #79352 | Amey | Fix cyclomatic complexity issue for Sidepanel.view file

--- src/features/SidePanel/SidePanel.view.tsx
+++ src/features/SidePanel/SidePanel.view.tsx
@@ -21,7 +21,6 @@ import { MatchPermissions, Permission, authService } from "@essnextgen/auth-ui";
 import { SidePanelProps, QuickLinkSidePanel } from "./SidePanelProps";
 import { fetchQuickLinkDetails } from "../../shared/components/QuickLink/Quicklinkresponse";
 import { FetchQuickLinkpost } from "../../shared/services/quickLinkDomain/quickLinkService";
-import { IFetchQuickLinkDetailsFunctionResponse } from "../../shared/model/quickLink/responsemodels";
 import gtmAnalytics from "../../shared/utils/analytics";
 import { envConfig } from "../../shared/utils";
 
@@ -39,8 +38,8 @@ const SidePanel: React.FC<SidePanelProps> = ({
   showQuickLinkView,
   quicklinkData,
   setQuickLinkData,
-  isLoader,
-  isSIMSIDAdmin
+  isLoader= false,
+  isSIMSIDAdmin = false
 }) => {
   const [isError, setIsError]: [
     boolean,
@@ -51,236 +50,264 @@ const SidePanel: React.FC<SidePanelProps> = ({
     MatchPermissions.all
   );
   const fullName: string | null = authService.getUsername();
-  const loginFullname: string = (fullName === null) ? "" : fullName.split(" ")[0];
+  const loginFullname: string = getLoginFullName(fullName);
   const isMobileView: boolean = useMediaQuery(
     "(min-width:320px) and (max-width: 1023.9px)"
   );
-  const handleStarClick: (
+
+  const handleStarClick = async (
     id: number,
     favorite: boolean,
     name: string
-  ) => Promise<void> = async (id: number, favorite: boolean, name: string) => {
-
+  ): Promise<void> => {
     try {
-      const { status }: { status: number } = await FetchQuickLinkpost(
-        id,
-        favorite
-      );
-      if (status === 200) {
-        const responseapidata:
-          | IFetchQuickLinkDetailsFunctionResponse
-          | null
-          | undefined = await fetchQuickLinkDetails();
-        if (responseapidata != null) {
-          setQuickLinkData(responseapidata.response);
-
-        }
-      }
-
-      type ElementType = "empty_star" | "filled_star";
-      const elementType: ElementType = favorite ? "empty_star" : "filled_star";
-
-      gtmAnalytics.pushEvent({
-        event: "interact_click",
-        elementType,
-        elementTextOrLabel: name,
-        elementLocation: "sidebar"
-      });
+      await handleStarClickAsync(id, favorite, name, setQuickLinkData);
     } catch (error) {
       setIsError(true);
     }
   };
 
-  /*eslint-disable */
   return (
-    <>
-      <div
-        className={`side-view-dertfsg11463f ${isOpen ? "open-dertfsg11463f open-panel-dertfsg11463f side-view-res-dertfsg11463f" : "side-view-closed-dertfsg11463f"
-          }`}
-      >
-        {isOpen ? (
-          isMobileView || !isOpen ? (
-            <SideNavigationPanel
-              title={
-                loginFullname && loginFullname.length > 24 ? (
-                  <Tooltip
-                    dataTestId="test-id"
-                    align={TooltipAlign.Center}
-                    position={TooltipPosition.Bottom}
-                    content={loginFullname}
-                  >
-                    <span className="quick-link-username-tooltip-dertfsg11463f">
-                      {loginFullname.substring(0, 24) + "..."}
-                    </span>
-                  </Tooltip>
-                ) : (
-                  <span className="quick-link-username-tooltip-dertfsg11463f">
-                    <strong>{loginFullname}</strong>
-                  </span>
-                )
-              }
-              isOpen={isOpen}
-              onClose={togglePanel}
-            >
-              <div className="sidepanel-mobile-divider-dertfsg11463f">
-                <Divider
-                  as="li"
-                  dataTestId="panel-divider"
-                  id="panel-divider"
-                  orientation={Orientation.HORIZONTAL}
-                  role="separator"
-                />
-              </div>
-              <SideNavigationPanelContent>
-                <div>
-                  <div className="quick-lint-display-dertfsg11463f">
-                    {isMobileView ? (
-                      ""
-                    ) : (
-                      <span className="icon-close-dertfsg11463f">
-                        {" "}
-                        <Icon
-                          color={IconColor.Primary500}
-                          dataTestId="btn-90"
-                          id="variable-2"
-                          name="close"
-                          onClick={closePanel}
-                          size={24}
-                        />
-                      </span>
-                    )}
-                  </div>
-                  {isSIMSIDAdmin ? (
-                    simsIdAdminQuickLink()
-                  ) : (
-                    quickLink({
-                      isPermissionquicklink,
-                      isError,
-                      quicklinkData,
-                      handleStarClick,
-                      showQuickLinkView,
-                      isLoader,
-                      togglePanel,
-                      isMobileView,
-                    })
-                  )}
-                </div>
-              </SideNavigationPanelContent>
-            </SideNavigationPanel>
-          ) : (
-            <div>
-              <div className="quick-lint-display-dertfsg11463f">
-                {loginFullname && loginFullname.length > 24 ? (
-                  <Tooltip
-                    dataTestId="test-id"
-                    align={TooltipAlign.Center}
-                    position={TooltipPosition.Bottom}
-                    content={<span>{loginFullname}</span>}
-                  >
-                    <span className="quick-link-username-dertfsg11463f">
-                      <strong>{loginFullname}</strong>
-                    </span>
-                  </Tooltip>
-                ) : (
-                  <span className="quick-link-username-dertfsg11463f">
-                    <strong>{loginFullname}</strong>
-                  </span>
-                )}
-
-                <span className="icon-close-dertfsg11463f">
-                  {" "}
-                  <Icon
-                    color={IconColor.Primary500}
-                    dataTestId="btn-90"
-                    id="variable-2"
-                    name="close"
-                    onClick={closePanel}
-                    size={24}
-                  />
-                </span>
-              </div>
-              {isSIMSIDAdmin ? (
-                simsIdAdminQuickLink()
-              ) : (
-                quickLink({
-                  isPermissionquicklink,
-                  isError,
-                  quicklinkData,
-                  handleStarClick,
-                  showQuickLinkView,
-                  isLoader,
-                  togglePanel,
-                  isMobileView,
-                })
-              )}
-            </div>
-          )
-        ) : (
-          <div
-            className="open-panel-dertfsg11463f essui-open-panel-filled filled-icon-align"
-            data-testId="close-panel"
-          >
-            <Button
-              className="base-class"
-              color={ButtonColor.Utility}
-              dataTestId="btn-collapse"
-              iconColor={IconColor.Neutral800}
-              iconName="open-panel--left--filled"
-              onClick={togglePanel}
-              size={ButtonSize.Small}
-            />
-          </div>
-        )}
-        <div />
-      </div>
-    </>
-  );
-};
-
-
-const simsIdAdminQuickLink: () => JSX.Element = () => {
-  return (
-    <div className="left-sidepanel-home113">
-      <div className="quick-link-dertfsg11463f">
-        Quick links
-      </div>
-      <div className="quick-link-padding-dertfsg11463f ">
-        <div
-          className="quick-panel-cont-dertfsg11463f label-css"
-          key={1}
-          onClick={() => {
-            window.location.href = `${envConfig.INVITE_STAFF_URL}`;
-          }}
-          style={{ cursor: "pointer" }}
-        >
-          Invite Staff
-        </div>
-      </div>
+    <div className={`side-view-dertfsg11463f ${getSideViewClass(isOpen)}`}>
+      {isOpen ? (
+        renderSideNavigationPanel(
+          isMobileView,
+          loginFullname,
+          togglePanel,
+          closePanel,
+          isSIMSIDAdmin,
+          isPermissionquicklink,
+          isError,
+          quicklinkData,
+          handleStarClick,
+          showQuickLinkView,
+          isLoader,
+          isOpen
+        )
+      ) : (
+        renderClosedPanel(togglePanel)
+      )}
+      <div />
     </div>
   );
 };
+
+// Helper Functions
+
+const getLoginFullName = (fullName: string | null): string =>
+  fullName === null ? "" : fullName.split(" ")[0];
+
+const getSideViewClass = (isOpen: boolean): string =>
+  isOpen ? "open-dertfsg11463f open-panel-dertfsg11463f side-view-res-dertfsg11463f" : "side-view-closed-dertfsg11463f";
+
+const handleStarClickAsync = async (
+  id: number,
+  favorite: boolean,
+  name: string,
+  setQuickLinkData: React.Dispatch<React.SetStateAction<any>>
+) => {
+  const { status } = await FetchQuickLinkpost(id, favorite);
+  if (status === 200) {
+    const responseapidata = await fetchQuickLinkDetails();
+    if (responseapidata != null) {
+      setQuickLinkData(responseapidata.response);
+    }
+  }
+  const elementType = favorite ? "empty_star" : "filled_star";
+  gtmAnalytics.pushEvent({
+    event: "interact_click",
+    elementType,
+    elementTextOrLabel: name,
+    elementLocation: "sidebar",
+  });
+};
+
+const renderSideNavigationPanel = (
+  isMobileView: boolean,
+  loginFullname: string,
+  togglePanel: () => void,
+  closePanel: () => void,
+  isSIMSIDAdmin: boolean,
+  isPermissionquicklink: boolean,
+  isError: boolean,
+  quicklinkData: any,
+  handleStarClick: (
+    id: number,
+    favorite: boolean,
+    name: string
+  ) => Promise<void>,
+  showQuickLinkView: () => void,
+  isLoader: boolean,
+  isOpen: boolean
+) => (
+  isMobileView || !isOpen ? (
+    <SideNavigationPanel
+      title={getNavigationPanelTitle(loginFullname)}
+      isOpen={isOpen}
+      onClose={togglePanel}
+    >
+      <div className="sidepanel-mobile-divider-dertfsg11463f">
+        <Divider
+          as="li"
+          dataTestId="panel-divider"
+          id="panel-divider"
+          orientation={Orientation.HORIZONTAL}
+          role="separator"
+        />
+      </div>
+      <SideNavigationPanelContent>
+        <div>
+          <div className="quick-lint-display-dertfsg11463f">
+            {isMobileView ? "" : renderCloseIcon(closePanel)}
+          </div>
+          {isSIMSIDAdmin
+            ? simsIdAdminQuickLink()
+            : quickLink({
+                isPermissionquicklink,
+                isError,
+                quicklinkData,
+                handleStarClick,
+                showQuickLinkView,
+                isLoader,
+                togglePanel,
+                isMobileView,
+              })}
+        </div>
+      </SideNavigationPanelContent>
+    </SideNavigationPanel>
+  ) : (
+    renderQuickLinkContent(
+      loginFullname,
+      closePanel,
+      isSIMSIDAdmin,
+      isPermissionquicklink,
+      isError,
+      quicklinkData,
+      handleStarClick,
+      showQuickLinkView,
+      isLoader,
+      togglePanel,
+      isMobileView
+    )
+  )
+);
+
+const getNavigationPanelTitle = (loginFullname: string): JSX.Element =>
+  loginFullname && loginFullname.length > 24 ? (
+    <Tooltip
+      dataTestId="test-id"
+      align={TooltipAlign.Center}
+      position={TooltipPosition.Bottom}
+      content={loginFullname}
+    >
+      <span className="quick-link-username-dertfsg11463f">
+      {`${loginFullname.substring(0, 24)}...`}
+      </span>
+    </Tooltip>
+  ) : (
+    <span className="quick-link-username-dertfsg11463f">
+      <strong>{loginFullname}</strong>
+    </span>
+  );
+
+const renderCloseIcon = (closePanel: () => void): JSX.Element => (
+  <span className="icon-close-dertfsg11463f">
+    <Icon
+      color={IconColor.Primary500}
+      dataTestId="btn-90"
+      id="variable-2"
+      name="close"
+      onClick={closePanel}
+      size={24}
+    />
+  </span>
+);
+
+const renderClosedPanel = (togglePanel: () => void): JSX.Element => (
+  <div
+    className="open-panel-dertfsg11463f essui-open-panel-filled filled-icon-align"
+    data-testId="close-panel"
+  >
+    <Button
+      className="base-class"
+      color={ButtonColor.Utility}
+      dataTestId="btn-collapse"
+      iconColor={IconColor.Neutral800}
+      iconName="open-panel--left--filled"
+      onClick={togglePanel}
+      size={ButtonSize.Small}
+    />
+  </div>
+);
+
+const renderQuickLinkContent = (
+  loginFullname: string,
+  closePanel: () => void,
+  isSIMSIDAdmin: boolean,
+  isPermissionquicklink: boolean,
+  isError: boolean,
+  quicklinkData: any,
+  handleStarClick: (
+    id: number,
+    favorite: boolean,
+    name: string
+  ) => Promise<void>,
+  showQuickLinkView: () => void,
+  isLoader: boolean,
+  togglePanel: () => void,
+  isMobileView: boolean
+): JSX.Element => (
+  <div>
+    <div className="quick-lint-display-dertfsg11463f">
+      {getNavigationPanelTitle(loginFullname)}
+      {renderCloseIcon(closePanel)}
+    </div>
+    {isSIMSIDAdmin
+      ? simsIdAdminQuickLink()
+      : quickLink({
+          isPermissionquicklink,
+          isError,
+          quicklinkData,
+          handleStarClick,
+          showQuickLinkView,
+          isLoader,
+          togglePanel,
+          isMobileView,
+        })}
+  </div>
+);
+
+const simsIdAdminQuickLink = () => (
+  <div className="left-sidepanel-home113">
+    <div className="quick-link-dertfsg11463f">
+      Quick links
+    </div>
+    <div className="quick-link-padding-dertfsg11463f ">
+      <div
+        className="quick-panel-cont-dertfsg11463f label-css"
+        key={1}
+        onClick={() => {
+          window.location.href = `${envConfig.INVITE_STAFF_URL}`;
+        }}
+        style={{ cursor: "pointer" }}
+      >
+        Invite Staff
+      </div>
+    </div>
+  </div>
+);
 //  const getUsernameTooltip :(loginFullname: any) => JSX.Element= (loginFullname) => (
-const quickLink: ({
-  isPermissionquicklink,
-  isError,
-  quicklinkData,
-  handleStarClick,
-  showQuickLinkView,
-  isLoader,
-  togglePanel,
-  isMobileView
-}: QuickLinkSidePanel) => JSX.Element = ({
-  isPermissionquicklink,
-  isError,
-  quicklinkData,
-  handleStarClick,
-  showQuickLinkView,
-  isLoader,
-  togglePanel,
-  isMobileView
-}) => {
-    return (
-      isPermissionquicklink && (
+  const quickLink = ({
+    isPermissionquicklink,
+    isError,
+    quicklinkData,
+    handleStarClick,
+    showQuickLinkView,
+    isLoader,
+    togglePanel,
+    isMobileView
+  }: QuickLinkSidePanel) => (
+    isPermissionquicklink && (
         <div className="left-sidepanel-home113">
           <div className="quick-link-dertfsg11463f">Quick links</div>
           <div className="quick-link-padding-dertfsg11463f">
@@ -359,7 +386,6 @@ no-script-url
         </div>
       )
     );
-  };
 
 /* eslint-enable */
 export default SidePanel;
