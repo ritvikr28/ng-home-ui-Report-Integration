b8e8f51136dd4c74d0fad439a7b5d81b8076647c
Secret Scanner <secret.scanner>
2024-09-05 15:40:57 +0530

    #80327 | Gulafsha | Integrate UAM Package component for testing

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -25,35 +25,17 @@ import { saveAppPermission, startRequest } from "./actions/storeActions";
 import { IAppModule } from "./types/AppPermission";
 import getAppModulesPermissions from "./actions/queries";
 import NewHomepageView from "./pages/NewHomePage/NewHomePage.view";
-import { envConfig, getUserOrganisation, isAuthzUserAdmin, service } from "./shared/utils";
-import SLTmockpage from "./features/SLTView/SLTmockpage";
+import { envConfig, isAuthzUserAdmin, service } from "./shared/utils";
+
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import AdminConsole from "./features/AdminConsole/AdminConsole.view";
 import SIMSIDAdminPageView from "./pages/SIMSIDAdminPage/SIMSIDAdminPage.view";
 import UnAuthorisedAccess from "./pages/AdminConsoleNoAccess/AdminConsoleNoAccess.view";
+import UAM from "./features/AdminConsole/UAM.view";
 
 
 
-const organisationId = [ "4b4eb751-c3f1-4a95-aade-d762b6c70693",
-"29a88689-e51f-4928-aead-1a92402c1a09",
-"54dbb8a7-9a07-48c5-92a3-014f13519f1d",
-"133ba2ce-a183-4ef9-8db3-f073d7941660",
-"d1ac710d-a8a4-4097-b30d-622c311dc535",
-"e6795699-4584-4829-89dd-41c67a1b1bbf",
-"ecb01589-7a16-44e3-8089-ebcdd9bc2458",
-"b8fd9320-6b94-40e5-bbcc-88efd5974951",
-"8e42af99-d37f-455b-bccd-20dffd8946ac",
-"6f039714-5c4a-4ab4-ad2e-f22e1145fcce",
-"ba8937a9-e63e-43ff-ada2-05a4eda64a6f",
-"f1d00baf-1bc3-4e43-a3fd-ba8cd59465d3",
-"c365ab78-ebba-4956-a2fa-11a967fb2cc6",
-"cff91875-22fc-463c-be4c-cec9e86b5752",
-"430c0edc-6482-476d-ae18-2eac23e57621",
-"cd0e52dd-8331-44dd-bea4-cf1e99d6e1fe",
-"6607c902-d51d-4200-9740-f230bb1bc8c1",
-"58df2bbd-1d47-4ea0-9613-aee79c442991",
-"4b4eb751-c3f1-4a95-aade-d762b6c70693",
-"ba8937a9-e63e-43ff-ada2-05a4eda64a6f"];
+
 
 const LandingPage: LazyExoticComponent<() => JSX.Element> = lazy(
   () => import("./pages/LandingPage")
@@ -150,6 +132,10 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
     `${envConfig.APPLICATION}`,
     "AdminConsoleView"
   );
+  const hasUAMPermission: boolean = hasFeaturePermission(
+    `${envConfig.APPLICATION}`,
+    "UAMView"
+  );
   
   const requiredNewHomePagePermissions: Permission[] = [
     {
@@ -202,7 +188,7 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
       history.push("/auth");
     }
   };
-  const shouldRenderSLTView: boolean = organisationId.includes(getUserOrganisation());
+  
 
   return (
     /* eslint-disable react/prop-types */
@@ -234,7 +220,8 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
           <ProtectedRoute exact path="/noAccess" component={NoAccess} />
           <ProtectedRoute exact path="/unauthorized" component={UnAuthorisedAccess} />
           {hasAdminConsoleFlagrPermission && <ProtectedRoute exact path="/AdminConsole" render={() => hasAdminConsolePermissions ? <AdminConsole /> : <Redirect to="/unauthorized"/>} />}
-          {shouldRenderSLTView && <ProtectedRoute exact path="/slt-view" component={SLTmockpage} />}
+          
+          {hasUAMPermission && <ProtectedRoute exact path="/uam" component={UAM} />}
           {isStandaloneApp && <Route exact path="/auth" component={Auth} />}
           <ProtectedRoute onAuthenticated={onAuthenticated}  exact  path="/schoolRedirect/:id" render={() => <SchoolGroupRedirect />} />
           <ProtectedRoute exact path="*" component={PageNotFound} />
