40ccb4b3911ad69cc3cb24edd7d522788e2962c5
Bhagyashri NPatil <Bhagyashri.NPatil@parentpay.com>
2024-11-25 11:33:00 +0530

    #121942 | Bhagyashri | Fix kiuwan issue

---
+++ src/features/AdminConsole/RefreshDatabase/ConfirmationDialog.logic.tsx
@@ -0,0 +1,97 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  Button,
+  ButtonColor
+} from "@essnextgen/ui-kit";
+import { useTranslation } from "@essnextgen/ui-intl-kit";
+
+interface IProps {  
+  dataTestId?: string;
+  title: string;
+  description: string;
+  confirmActionButtonText?: string;
+  cancelActionButtonText?: string;
+  isOpen?: boolean;
+  optionalButton?: boolean;
+  optionalButtonText?: string;
+  onOptionalHandle?: () => void;
+  onSubmitHandle: () => void;
+  onCloseHandle: () => void;
+}
+
+export const handleDocumentBodyOverflow: () => void = () => {
+  const bodyNoScrollClass = "essui-body--no-scroll";
+
+  if (!document.body.classList.contains(bodyNoScrollClass)) {
+    return;
+  }
+
+  document.body.classList.remove(bodyNoScrollClass);
+};
+
+
+export const ConfirmDialog: ({}: IProps) => JSX.Element = ({
+  title,
+  description,
+  confirmActionButtonText = "Ok",
+  cancelActionButtonText = "Cancel",
+  dataTestId = "default-dialog",
+  isOpen,
+  optionalButton,
+  onOptionalHandle,
+  onCloseHandle,
+  onSubmitHandle
+}: IProps): JSX.Element => {
+  const { t } = useTranslation();
+
+  const handleOnSubmit: () => void = () => {
+    handleDocumentBodyOverflow();
+    onSubmitHandle();
+  };
+
+  const handleOnClose: () => void = () => {
+    handleDocumentBodyOverflow();
+    onCloseHandle();
+  };
+
+  return (
+    <Dialog
+      isOpen={isOpen}
+      dataTestId={dataTestId}
+      escapeExits
+      onClose={handleOnClose}
+      title={title}
+    >
+      <DialogContent>{description}</DialogContent>
+        <DialogFooter>
+          <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '20px' }}>
+          
+            {optionalButton && (
+              <span style={{ marginLeft: '10px' }}>
+                <Button
+                  dataTestId={`${dataTestId}-close-btn`}
+                  onClick={handleOnClose}
+                  color={ButtonColor.Secondary}
+              >
+                {cancelActionButtonText}
+              </Button>
+              </span>
+            )}
+            <Button
+                  dataTestId={`${dataTestId}-ok-btn`}
+                  onClick={() => {
+                    handleOnSubmit();
+                    handleOnClose();
+                  }}
+                  color={ButtonColor.Primary}
+                  >
+                  {confirmActionButtonText}
+              </Button>
+            </div>
+        </DialogFooter>
+    </Dialog>
+  );
+};
+export default ConfirmDialog;
