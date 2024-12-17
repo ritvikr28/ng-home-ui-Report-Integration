cd3dd71e1e6ade71b96b9c21b867acb520bc6472
Suraj Bawankar <Suraj.Bawankar@parentpay.com>
2024-12-17 15:52:03 +0530

    #122964 | Suraj | Updated Kuiwan fixes

--- src/shared/utils/errorHandler.ts
+++ src/shared/utils/errorHandler.ts
@@ -1,6 +1,6 @@
-export const handle401Error = (statusCode: number) => {
-    if (statusCode === 401) {
-      console.error("Unauthorized access detected. Redirecting to /unauthorized...");
-      window.location.href = "/unauthorized"; // Redirect to Unauthorized page
-    }
-  };
+export const handle401Error = (statusCode: number): void => {
+  if (statusCode === 401) {
+    console.error("Unauthorized access detected. Redirecting to /unauthorized...");
+    window.location.href = "/unauthorized"; // Redirect to Unauthorized page
+  }
+};
