f9a2cb845ae4026a5acb855f6a43f4097a4ca75e
Gauri Tayade <Gauri.Tayade@parentpay.com>
2025-04-06 19:03:05 +0530

    #112522 | Gauri | Fix cyclomatic issue.

--- src/features/MainPanel/TakeRegisters/component/EventContainer/TakeRegisterEvent.view.tsx
+++ src/features/MainPanel/TakeRegisters/component/EventContainer/TakeRegisterEvent.view.tsx
@@ -1,14 +1,11 @@
 import React, { useRef, useEffect, useState } from "react";
-import Carousel from "react-multi-carousel";
 import {
-  ActionCard,
   Button,
   ButtonColor,
   ButtonSize,
   Grid,
   GridItem,
   IconColor,
-  TagColor,
   useMediaQuery
 } from "@essnextgen/ui-kit";
 import {
@@ -20,14 +17,11 @@ import { IRegisterViewProps } from "./props";
 import "./carousalstyle.scss";
 import { responsive, iscloseresponsive } from "./carousel";
 import {
-  envConfig,
-  getUser,
-  getUserOrganisation
+  envConfig
 } from "../../../../../shared/utils";
-import { IRegistersDetails } from "../../../../../shared/model/RegisterDomain/responsemodels";
-import gtmAnalytics from "../../../../../shared/utils/analytics";
-import { logger } from "../../../../../shared/components/AppInsights";
 import { SectionTitle } from "../../../../../shared/components/SectionTitle/SectionTitle";
+import { handleRegisterClick, isButtonDisabled, nextSlide, previousSlide, renderCarousel, renderNoRegisterMessage, setDefaultAndCurrentSlide } from "./TakeRegisterEventHelper";
+
 
 
 const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
@@ -42,163 +36,27 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
     apiRegsiterEventData ? 0 : 0
   );
   const [carouselData, setCarouselData] = useState(responsive);
-  const setDefaultSlide = (index: number) => {
-    /* istanbul ignore next */
-    carouselRef.current.goToSlide(index);
-  };
 
   useEffect(() => {
     /* eslint-disable */
-    isOpen ? setCarouselData(responsive) : setCarouselData(iscloseresponsive);
+    setCarouselData(isOpen ? responsive : iscloseresponsive);
   }, [isOpen]);
 
   /* eslint-enable */
 
   const isMediumscreen: boolean = useMediaQuery("(min-width:1439.9px)");
   useEffect(() => {
-    if (apiRegsiterEventData != null && apiRegsiterEventData.length > 0) {
-      if (carouselRef && carouselRef.current && !effectTriggered) {
-        const Index = apiRegsiterEventData.findIndex((x) => {
-          if (x.eventStart != null && x.eventEnd != null) {
-            const formattedLocalTime = new Date();
-            const currentUTCDateTime = formattedLocalTime
-              .toISOString()
-              .split(".")[0];
-            return (
-              (Date.parse(x.eventStart) <= Date.parse(currentUTCDateTime) &&
-                Date.parse(currentUTCDateTime) <= Date.parse(x.eventEnd)) ||
-              Date.parse(x.eventStart) > Date.parse(currentUTCDateTime)
-            );
-          }
-          /* istanbul ignore next */
-          return -1;
-        });
+    if (!apiRegsiterEventData?.length || effectTriggered || !carouselRef?.current) return;
 
-        setEffectTriggered(true);
-        if (Index < 0) {
-          setDefaultSlide(apiRegsiterEventData.length);
-          setCurrentSlide(apiRegsiterEventData.length - 1);
-        } else {
-          setDefaultSlide(Index);
-          if (apiRegsiterEventData.length - Index <= 3)
-            setCurrentSlide(apiRegsiterEventData.length - 1);
-          else setCurrentSlide(Index);
-        }
-      }
-    }
-  });
-  /* istanbul ignore next */
-  const nextSlide = () => {
-    if (carouselRef.current) {
-      carouselRef.current.next();
-      moveRight();
-    }
-  };
-  /* istanbul ignore next */
-  const moveRight = () => {
-    const totalLength = apiRegsiterEventData ? apiRegsiterEventData.length : 0;
+    setEffectTriggered(true);
 
-    setCurrentSlide((prevSlide) => {
-      if (window.innerWidth < 768) {
-        return prevSlide + 1 >= totalLength ? totalLength : prevSlide + 1;
-      }
-      if (window.innerWidth >= 768 && window.innerWidth < 1440) {
-        return prevSlide + 2 >= totalLength ? totalLength : prevSlide + 2;
-      }
-      return prevSlide + 3 >= totalLength ? totalLength : prevSlide + 3;
-    });
-  };
-
-  /* istanbul ignore next */
-  const moveLeft = () => {
-    setCurrentSlide((prevSlide) => {
-      if (window.innerWidth < 768) {
-        return Math.max(prevSlide - 1, 0);
-      }
-      if (window.innerWidth >= 768 && window.innerWidth < 1440) {
-        return Math.max(prevSlide - 2, 0);
-      }
-      return Math.max(prevSlide - 3, 0);
-    });
-  };
-
-  /* istanbul ignore next */
-  const previousSlide = () => {
-    if (carouselRef.current && currentSlide > 0) {
-      carouselRef.current.previous();
-      moveLeft();
-    }
-  };
-
-  /* istanbul ignore next */
-  const FilledGraphDataIcon = () => (
-    /* istanbul ignore next */
-    <svg
-      width="54"
-      height="54"
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M14 21V27"
-        stroke="#18A0FB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M18 14V27"
-        stroke="#18A0FB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M22 16V27"
-        stroke="#18A0FB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M26 20V27"
-        stroke="#18A0FB"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <circle
-        opacity="0.05"
-        cx="20"
-        cy="20"
-        r="20"
-        transform="rotate(-90 20 20)"
-        fill="#18A0FB"
-      />
-    </svg>
-  );
-
-  const OnRegisterClick = (item: IRegistersDetails) => {
-    const url =
-      item.eventTypeCode === "AttendanceSession"
-        ? `${envConfig.REGISTER_BASE_URL}/take-register/${item.eventDescription}/${item.group.externalId}/${item.eventInstanceExternalId}`
-        : `${envConfig.REGISTER_BASE_URL}/take-register/${item.classPeriodExternalId}/${item.group.externalId}/${item.eventInstanceExternalId}`;
-
-    logger.info(
-      `Click on registers -${url} organisationId- ${getUserOrganisation()} userId- ${getUser()}`
+    setDefaultAndCurrentSlide(
+      carouselRef,
+      apiRegsiterEventData,
+      setCurrentSlide
     );
+   }, [apiRegsiterEventData, effectTriggered]);
 
-    gtmAnalytics.pushEvent({
-      event: "click",
-      linkText: "[RemovedClassName]",
-      linkUrl: url,
-      clickType: "card",
-      clickLocation: "body",
-    });
-    /* istanbul ignore next */
-    window.open(url, "_self");
-  };
   return (
     <>
       <Grid className="register-icon">
@@ -206,7 +64,7 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
           <SectionTitle
             title={t("takeRegister.takeregisterlink")}
             hasLink
-            linkText= {t("takeRegister.takeregistertext")}
+            linkText={t("takeRegister.takeregistertext")}
             linkhref={envConfig.REGISTER_BASE_URL}
           />
         </GridItem>
@@ -218,20 +76,16 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
             color={ButtonColor.Utility}
             ariaLabel="carousel-left-btn"
             iconName="chevron--left"
-            onClick={previousSlide}
+            onClick={() => previousSlide(carouselRef, currentSlide, setCurrentSlide)}
             size={ButtonSize.Small}
             type="button"
             /* eslint-disable */
-            disabled={
-              apiRegsiterEventData == null
-                ? true
-                : window.innerWidth > 768 && window.innerWidth < 1440
-                ? currentSlide === 0 || apiRegsiterEventData.length < 2
-                : window.innerWidth < 768
-                ? currentSlide === 0 || apiRegsiterEventData.length < 1
-                : currentSlide === 0 || apiRegsiterEventData.length < 4
-            }
-            /* eslint-enable */
+            disabled={isButtonDisabled(
+              "previous",
+              apiRegsiterEventData ?? null,
+              currentSlide
+            )}
+          /* eslint-enable */
           />
           <Button
             className="base-class"
@@ -240,114 +94,32 @@ const TakeRegisterEventView: React.FC<IRegisterViewProps> = ({
             color={ButtonColor.Utility}
             iconName="chevron--right"
             ariaLabel="carousel-right-btn"
-            onClick={nextSlide}
+            onClick={() => nextSlide(carouselRef, apiRegsiterEventData ?? null, setCurrentSlide)}
             size={ButtonSize.Small}
             type="button"
             /* eslint-disable */
-            disabled={
-              apiRegsiterEventData == null
-                ? true
-                : window.innerWidth > 768 && window.innerWidth < 1440
-                ? currentSlide === (apiRegsiterEventData.length ?? 0) - 1 ||
-                  currentSlide + 2 > apiRegsiterEventData.length
-                : window.innerWidth < 768
-                ? currentSlide + 1 > apiRegsiterEventData.length
-                : currentSlide === (apiRegsiterEventData.length ?? 0) - 1 ||
-                  currentSlide + 3 >= apiRegsiterEventData.length
-              /* eslint-enable */
-            }
+            disabled={isButtonDisabled(
+              "next",
+              apiRegsiterEventData ?? null,
+              currentSlide
+            )}
           />
         </GridItem>
       </Grid>
-
       <div>
         {apiError === false &&
-        apiRegsiterEventData &&
-        apiRegsiterEventData.length > 0 ? (
-          <Carousel
-            ref={carouselRef}
-            slidesToSlide={window.innerWidth <= 768 ? 1 : 4}
-            arrows={false}
-            swipeable={false}
-            draggable={false}
-            showDots={false}
-            responsive={carouselData}
-            infinite={false}
-            keyBoardControl
-            customTransition="all .5"
-            transitionDuration={50}
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["tablet", "mobile"]}
-            itemClass="carousel-item-padding-40-px"
-          >
-            {apiRegsiterEventData &&
-              apiRegsiterEventData.map((item, index) => (
-                <div
-                  key={`key-${index}`}
-                  className="new-action-card-accodian-container"
-                >
-                  <ActionCard
-                    dataTestId={`test-id${index}`}
-                    icon={<FilledGraphDataIcon />}
-                    id={`action-card${index}`}
-                    onClickActionCard={() => {
-                      OnRegisterClick(item);
-                    }}
-                    primaryText={
-                      `${item.group.shortName!} ${
-                        item.room ? ` | ${item.room.roomName!}` : ""
-                      }`.length > 16 && isMediumscreen
-                        ? `${(
-                            item.group.shortName! +
-                            (item.room ? ` | ${item.room.roomName!}` : "")
-                          ).substring(0, 16)}...`
-                        : `${item.group.shortName!} ${
-                            item.room ? ` | ${item.room.roomName!}` : ""
-                          }`
-                    }
-                    tagText={item.isCompleted ? t("takeregister.completed") : t("takeregister.ready")}
-                    isShowTag
-                    tagColor={
-                      item.isCompleted ? TagColor.Success : TagColor.Outstanding
-                    }
-                    isShowArrowIcon
-                  />
-                </div>
-              ))}
-            {apiRegsiterEventData.length > 0 && (
-              <div
-                className="new-action-card-accodian-container noregister eventcardnohighlight"
-              >
-                <ActionCard
-                  dataTestId="test-id"
-                  icon={<FilledGraphDataIcon />}
-                  id="no-more-register-id"
-                  onClickActionCard={() => {}}
-                  primaryText={t("takeregister.nomoreregister")} 
-                  isShowArrowIcon={false}
-                />
-              </div>
-            )}
-          </Carousel>
-        ) : (
-          apiError === false &&
-          (apiRegsiterEventData == null ||
-            apiRegsiterEventData.length === 0) && (
-            <div
-              className="new-action-card-accodian-container carousel-item-padding-40-px noregisterblock no-register-box eventcardnohighlight"
-            >
-              <ActionCard
-                dataTestId="no-test-id"
-                icon={<></>}
-                id="no-register-id"
-                onClickActionCard={() => {}}
-                primaryText= {t("takeregister.noregistertoday")}
-                isShowArrowIcon={false}
-              />
-            </div>
-          )
-        )}
+          ((apiRegsiterEventData ?? []).length > 0
+            ? renderCarousel(
+                apiRegsiterEventData ?? [],
+                isMediumscreen,
+                carouselRef,
+                carouselData,
+                handleRegisterClick,
+                t
+              )
+            : renderNoRegisterMessage(t))}
       </div>
+      
     </>
   );
 };
---
+++ src/features/MainPanel/TakeRegisters/component/EventContainer/TakeRegisterEventHelper.tsx
@@ -0,0 +1,290 @@
+import Carousel from "react-multi-carousel";
+import { ActionCard, TagColor } from "@essnextgen/ui-kit";
+import React from "react";
+import { envConfig, getUser, getUserOrganisation } from "../../../../../shared/utils";
+import gtmAnalytics from "../../../../../shared/utils/analytics";
+import { logger } from "../../../../../shared/components/AppInsights";
+import { IRegistersDetails } from "../../../../../shared/model/RegisterDomain/responsemodels";
+
+
+
+export const setDefaultAndCurrentSlide = (
+  carouselRef: any,
+  apiRegsiterEventData: IRegistersDetails[],
+  setCurrentSlide: React.Dispatch<React.SetStateAction<number>>
+): void => {
+  const Index = findCurrentIndex(apiRegsiterEventData);
+
+  const isLastFewSlides =
+    Index >= 0 && apiRegsiterEventData.length - Index <= 3;
+
+  carouselRef.current?.goToSlide(
+    Index < 0 ? apiRegsiterEventData.length : Index
+  );
+
+  setCurrentSlide(
+    Index < 0
+      ? apiRegsiterEventData.length - 1
+      : isLastFewSlides
+      ? apiRegsiterEventData.length - 1
+      : Index
+  );
+};
+
+export const renderCarousel = (
+  apiRegsiterEventData: IRegistersDetails[],
+  isMediumscreen: boolean,
+  carouselRef: any,
+  carouselData: any,
+  handleRegisterClick: (item: IRegistersDetails) => void,
+  t: any
+) => (
+  <Carousel
+    ref={carouselRef}
+    slidesToSlide={window.innerWidth <= 768 ? 1 : 4}
+    arrows={false}
+    swipeable={false}
+    draggable={false}
+    showDots={false}
+    responsive={carouselData}
+    infinite={false}
+    keyBoardControl
+    customTransition="all .5"
+    transitionDuration={50}
+    containerClass="carousel-container"
+    removeArrowOnDeviceType={["tablet", "mobile"]}
+    itemClass="carousel-item-padding-40-px"
+  >
+    {apiRegsiterEventData.map((item, index) => (
+      <div
+        key={`key-${index}`}
+        className="new-action-card-accodian-container"
+      >
+        <ActionCard
+          dataTestId={`test-id${index}`}
+          icon={<FilledGraphDataIcon />}
+          id={`action-card${index}`}
+          onClickActionCard={() => handleRegisterClick(item)}
+          primaryText={getPrimaryText(item, isMediumscreen)}
+          tagText={
+            item.isCompleted
+              ? t("takeregister.completed")
+              : t("takeregister.ready")
+          }
+          isShowTag
+          tagColor={
+            item.isCompleted ? TagColor.Success : TagColor.Outstanding
+          }
+          isShowArrowIcon
+        />
+      </div>
+    ))}
+    <div className="new-action-card-accodian-container noregister eventcardnohighlight">
+      <ActionCard
+        dataTestId="test-id"
+        icon={<FilledGraphDataIcon />}
+        id="no-more-register-id"
+        onClickActionCard={() => {}}
+        primaryText={t("takeregister.nomoreregister")}
+        isShowArrowIcon={false}
+      />
+    </div>
+  </Carousel>
+);
+
+export const renderNoRegisterMessage = (t: any) => (
+  <div className="new-action-card-accodian-container carousel-item-padding-40-px noregisterblock no-register-box eventcardnohighlight">
+    <ActionCard
+      dataTestId="no-test-id"
+      icon={<></>}
+      id="no-register-id"
+      onClickActionCard={() => {}}
+      primaryText={t("takeregister.noregistertoday")}
+      isShowArrowIcon={false}
+    />
+  </div>
+);
+
+export const nextSlide = (
+  carouselRef: any,
+  apiRegsiterEventData: IRegistersDetails[] | null,
+  setCurrentSlide: React.Dispatch<React.SetStateAction<number>>
+): void => {
+  if (carouselRef.current) {
+    carouselRef.current.next();
+    moveRight(apiRegsiterEventData, setCurrentSlide);
+  }
+};
+
+  export const moveRight = (
+    apiRegsiterEventData: IRegistersDetails[] | null,
+    setCurrentSlide: React.Dispatch<React.SetStateAction<number>>
+  ): void => {
+    const totalLength = apiRegsiterEventData?.length || 0;
+    setCurrentSlide((prevSlide) => calculateNextSlide(prevSlide, totalLength));
+  };
+  
+  export const moveLeft = (
+    setCurrentSlide: React.Dispatch<React.SetStateAction<number>>
+  ): void => {
+    setCurrentSlide((prevSlide) => calculatePreviousSlide(prevSlide));
+  };
+  
+  export const previousSlide = (
+    carouselRef: any,
+    currentSlide: number,
+    setCurrentSlide: React.Dispatch<React.SetStateAction<number>>
+  ): void => {
+    if (carouselRef.current && currentSlide > 0) {
+      carouselRef.current.previous();
+      moveLeft(setCurrentSlide);
+    }
+  };
+  
+  export const isButtonDisabled = (
+    direction: "previous" | "next",
+    apiRegsiterEventData: IRegistersDetails[] | null,
+    currentSlide: number
+  ): boolean => {
+    if (!apiRegsiterEventData?.length) return true;
+  
+    const slidesToShow = getSlidesToShow();
+    return direction === "previous"
+      ? currentSlide === 0
+      : currentSlide + slidesToShow >= apiRegsiterEventData.length;
+  };
+
+/**
+ * Generates the URL for the register event.
+ */
+export const generateRegisterUrl = (item: IRegistersDetails): string => {
+  return item.eventTypeCode === "AttendanceSession"
+    ? `${envConfig.REGISTER_BASE_URL}/take-register/${item.eventDescription}/${item.group.externalId}/${item.eventInstanceExternalId}`
+    : `${envConfig.REGISTER_BASE_URL}/take-register/${item.classPeriodExternalId}/${item.group.externalId}/${item.eventInstanceExternalId}`;
+};
+
+/**
+ * Handles the click event for a register item.
+ */
+export const handleRegisterClick = (item: IRegistersDetails): void => {
+  const url = generateRegisterUrl(item);
+
+  logger.info(
+    `Click on registers -${url} organisationId- ${getUserOrganisation()} userId- ${getUser()}`
+  );
+
+  gtmAnalytics.pushEvent({
+    event: "click",
+    linkText: "[RemovedClassName]",
+    linkUrl: url,
+    clickType: "card",
+    clickLocation: "body",
+  });
+
+  /* istanbul ignore next */
+  window.open(url, "_self");
+};
+
+ const FilledGraphDataIcon = () => (
+  <svg
+    width="54"
+    height="54"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M14 21V27"
+      stroke="#18A0FB"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M18 14V27"
+      stroke="#18A0FB"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M22 16V27"
+      stroke="#18A0FB"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M26 20V27"
+      stroke="#18A0FB"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <circle
+      opacity="0.05"
+      cx="20"
+      cy="20"
+      r="20"
+      transform="rotate(-90 20 20)"
+      fill="#18A0FB"
+    />
+  </svg>
+);
+
+const isEventInProgress = (eventStart: string, eventEnd: string, currentUTCDateTime: string): boolean => 
+      Date.parse(eventStart) <= Date.parse(currentUTCDateTime) &&
+      Date.parse(currentUTCDateTime) <= Date.parse(eventEnd);
+
+  
+  const isEventUpcoming = (eventStart: string, currentUTCDateTime: string): boolean => 
+     Date.parse(eventStart) > Date.parse(currentUTCDateTime);
+
+export const findCurrentIndex = (
+    apiRegsiterEventData: IRegistersDetails[]
+  ): number => {
+    const currentUTCDateTime = new Date().toISOString().split(".")[0];
+    return apiRegsiterEventData.findIndex(
+      (x) =>
+        x.eventStart != null &&
+        x.eventEnd != null &&
+        (isEventInProgress(x.eventStart, x.eventEnd, currentUTCDateTime) ||
+          isEventUpcoming(x.eventStart, currentUTCDateTime))
+    );
+  };
+
+  export const getSlidesToShow = (): number => {
+    if (window.innerWidth < 768) return 1;
+    if (window.innerWidth >= 768 && window.innerWidth < 1440) return 2;
+    return 3;
+  };
+
+  export const calculateNextSlide = (
+    currentSlide: number,
+    totalLength: number
+  ): number => {
+    const slidesToShow = getSlidesToShow(); // Use helper function
+    return Math.min(currentSlide + slidesToShow, totalLength);
+  };
+
+  export const calculatePreviousSlide = (currentSlide: number): number => {
+    const slidesToShow = getSlidesToShow(); // Use helper function
+    return Math.max(currentSlide - slidesToShow, 0);
+  };
+
+  export const getPrimaryText = (
+    item: IRegistersDetails,
+    isMediumscreen: boolean
+  ): string => {
+    const text = `${item.group.shortName!} ${
+      item.room ? ` | ${item.room.roomName!}` : ""
+    }`;
+  
+    return text.length > 16 && isMediumscreen
+      ? `${text.substring(0, 16)}...`
+      : text;
+  };
+  
+
+  
+  
