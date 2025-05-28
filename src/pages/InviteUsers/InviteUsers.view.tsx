484ad59c21c8b8da66e2744b25ce80da0af5be40
Varshini Yakkala <Varshini.Yakkala@parentpay.com>
2025-05-28 16:22:21 +0530

    [96326][Varshini][Css class names]

--- src/pages/InviteUsers/InviteUsers.view.tsx
+++ src/pages/InviteUsers/InviteUsers.view.tsx
@@ -182,175 +182,177 @@ const InviteUserView: React.FC<InviteUserProps> = (props) => {
             onItemClick={onBreadcrumbClick}
           />
         </div>
-        <ControlledList
-          className="invite-users-table"
-          dataTestId="invite-list-test-id"
-          isShowFirstElement
-          isBreadCrumbEnable={false}
-          resultNotFoundMessage="No data to display"
-          dynamictableIconName={
-            showErrorBanner ? "warning--alt" : "information"
-          }
-          sortingAlign="left"
-          sortingOnClickEvent={(
-            e: React.SyntheticEvent,
-            columnName: string
-          ) => {
-            setshowInvitationConflictBanner(false);
-            const apiColumnName =
-              columnName === "Name" ? "Forename" : "EmailId";
-            let newDirection = true;
-
-            if (sortBy === apiColumnName) {
-              newDirection = !sortDirection;
-            } else {
-              newDirection = true;
-              setSortBy(apiColumnName);
+        <div className="invite-users-table">
+          <ControlledList
+            className="invite-users-table"
+            dataTestId="invite-list-test-id"
+            isShowFirstElement
+            isBreadCrumbEnable={false}
+            resultNotFoundMessage="No data to display"
+            dynamictableIconName={
+              showErrorBanner ? "warning--alt" : "information"
             }
+            sortingAlign="left"
+            sortingOnClickEvent={(
+              e: React.SyntheticEvent,
+              columnName: string
+            ) => {
+              setshowInvitationConflictBanner(false);
+              const apiColumnName =
+                columnName === "Name" ? "Forename" : "EmailId";
+              let newDirection = true;
 
-            setSortBy(apiColumnName);
-            setSortDirection(newDirection);
-
-            inviteUsersSorting(
-              columnName,
-              newDirection,
-              setSortDirection,
-              setSortBy,
-              currentPage,
-              pageSize,
-              setUsersTableData,
-              setLoader,
-              setShowErrorBanner,
-              setshowInvitationConflictBanner
-            );
-          }}
-          dynamictableNoMsgColor={ValidationTextLevel.Warning}
-          isShowdynamictableNoMsg={showErrorBanner}
-          isAddEventBtnShow={false}
-          dynamicTableLoader={isLoader}
-          filterDDLOptions={filterOptions}
-          editSelectedBtnTitle="Edit selected"
-          headingText="Invite Users"
-          subHeadingText="Invite SIMS 7 users to access SIMS Next Gen"
-          id="Inviteusers-list"
-          filterDDLuseAutoWidth
-          filterDDLisSelected
-          filterDDLlabel="Invitation status"
-          filterDDLplaceholder="Select"
-          filterDDLselectedItem={{ text: "All", value: "All" }}
-          filterDDLdisabled={false}
-          isOnCloseSidepnl
-          isPagination
-          paginationCount={totalPage}
-          paginationOnChange={handlePageChange}
-          paginationPage={currentPage}
-          paginationMinCountToHideNextPreviousBtn={0}
-          searchHeadingText="Search user"
-          searchPlaceholderText="Text"
-          searchTerm=""
-          secondaryButtonTitle="Cancel"
-          showConfirmDialog
-          tableBodyData={usersTableData || []}
-          isShowEditSelectedBtn
-          isShowSearch
-          tableFirstColumnWidth="56px"
-          tableHeadersData={getTableHeadersData}
-          tableLastColumnWidth="10px"
-          editSelectedOptions={editSelectedOptions}
-          onEditSelectedBtnClick={() => {}}
-          onEditSelectedOverFlowMenu={(e: any) => {
-            if (e.target?.innerHTML === "Send invite") {
-              if (selectedCheckBoxIds.length === 0) {
-                setShowDialog(true);
+              if (sortBy === apiColumnName) {
+                newDirection = !sortDirection;
+              } else {
+                newDirection = true;
+                setSortBy(apiColumnName);
               }
-            }
-          }}
-          handleCloseDialogConfirmation={() => {
-            setShowConfirmDialog(false);
-          }}
-          templatePropsConfirmation={{
-            cancelText: "Cancel",
-            contentText:
-              "This user will be sent an invite to access the system.",
-            isNotificationanner: false,
-            notificationStatus: NotificationStatus.SUCCESS,
-            okText: "Save",
-            onCancel: (): void => {
-              setShowConfirmDialog(false);
-            },
-            onConfirm: (): void => {
-              setShowConfirmDialog(false);
-              handleSendInvite({
-                selectedRowItem,
+
+              setSortBy(apiColumnName);
+              setSortDirection(newDirection);
+
+              inviteUsersSorting(
+                columnName,
+                newDirection,
+                setSortDirection,
+                setSortBy,
+                currentPage,
+                pageSize,
+                setUsersTableData,
                 setLoader,
-                setShowInviteErrBanner,
-                setDataUpdated
+                setShowErrorBanner,
+                setshowInvitationConflictBanner
+              );
+            }}
+            dynamictableNoMsgColor={ValidationTextLevel.Warning}
+            isShowdynamictableNoMsg={showErrorBanner}
+            isAddEventBtnShow={false}
+            dynamicTableLoader={isLoader}
+            filterDDLOptions={filterOptions}
+            editSelectedBtnTitle="Edit selected"
+            headingText="Invite Users"
+            subHeadingText="Invite SIMS 7 users to access SIMS Next Gen"
+            id="Inviteusers-list"
+            filterDDLuseAutoWidth
+            filterDDLisSelected
+            filterDDLlabel="Invitation status"
+            filterDDLplaceholder="Select"
+            filterDDLselectedItem={{ text: "All", value: "All" }}
+            filterDDLdisabled={false}
+            isOnCloseSidepnl
+            isPagination
+            paginationCount={totalPage}
+            paginationOnChange={handlePageChange}
+            paginationPage={currentPage}
+            paginationMinCountToHideNextPreviousBtn={0}
+            searchHeadingText="Search user"
+            searchPlaceholderText="Text"
+            searchTerm=""
+            secondaryButtonTitle="Cancel"
+            showConfirmDialog
+            tableBodyData={usersTableData || []}
+            isShowEditSelectedBtn
+            isShowSearch
+            tableFirstColumnWidth="56px"
+            tableHeadersData={getTableHeadersData}
+            tableLastColumnWidth="10px"
+            editSelectedOptions={editSelectedOptions}
+            onEditSelectedBtnClick={() => {}}
+            onEditSelectedOverFlowMenu={(e: any) => {
+              if (e.target?.innerHTML === "Send invite") {
+                if (selectedCheckBoxIds.length === 0) {
+                  setShowDialog(true);
+                }
+              }
+            }}
+            handleCloseDialogConfirmation={() => {
+              setShowConfirmDialog(false);
+            }}
+            templatePropsConfirmation={{
+              cancelText: "Cancel",
+              contentText:
+                "This user will be sent an invite to access the system.",
+              isNotificationanner: false,
+              notificationStatus: NotificationStatus.SUCCESS,
+              okText: "Save",
+              onCancel: (): void => {
+                setShowConfirmDialog(false);
+              },
+              onConfirm: (): void => {
+                setShowConfirmDialog(false);
+                handleSendInvite({
+                  selectedRowItem,
+                  setLoader,
+                  setShowInviteErrBanner,
+                  setDataUpdated
+                });
+              },
+              template: DialogTemplate.Confirmation
+            }}
+            titleConfirmation="Invite user?"
+            toastNotificationStatus={NotificationStatus.SUCCESS}
+            toastNotificationTitle=""
+            isOpenConfirmationDialog={showConfirmDialog}
+            isShowOverflowMenuCol
+            globalNotificationBannerOnClickClose={() => {
+              setShowErrorBanner(false);
+              setShowInviteErrBanner(false);
+              setshowInvitationConflictBanner(false);
+            }}
+            globalNotificationMsgBannerObject={[
+              {
+                autoclose: false,
+                isShow: true,
+                message:
+                  "To manage user email name and role, please access SIMS7.",
+                title: "User details are managed in SIMS7",
+                variant: "highlight",
+                hideCloseButton: true
+              },
+              {
+                isShow: !!showErrorBanner,
+                variant: "warning",
+                title: "Information unavailable",
+                message:
+                  "A technical issue at our end has stopped us from displaying all information. Please try again later. If the issue persists, please get in touch with our support team.",
+                autoclose: true
+              },
+              {
+                isShow: !!showInvitationConflictBanner,
+                variant: "warning",
+                title: "Invitation conflict",
+                message:
+                  "There is an invitation conflict with some users on this list because they are associated with more than one SIMS ID account. Please contact our Service Desk team for assistance in resolving this issue.",
+                autoclose: true
+              },
+              {
+                isShow: !!showInviteErrBanner,
+                variant: "warning",
+                title: "Unable to save",
+                message:
+                  "A technical issue at our end has stopped us from saving the changes. Please try again. If the issue persists, please get in touch with our support team. We appreciate your patience and understanding during this time.",
+                autoclose: true
+              }
+            ]}
+            onClickOverflowItem={(e: any, selectedRow: any) => {
+              if (e.target?.innerHTML === "Send Invite") {
+                setShowConfirmDialog(true);
+                setSelectedRowItem(selectedRow);
+              }
+            }}
+            onChangeListCheckBox={(index: number, id: string) => {
+              handleCheckBoxSelection({
+                id,
+                selectedCheckBoxIds,
+                setSelectedCheckBoxIds
               });
-            },
-            template: DialogTemplate.Confirmation
-          }}
-          titleConfirmation="Invite user?"
-          toastNotificationStatus={NotificationStatus.SUCCESS}
-          toastNotificationTitle=""
-          isOpenConfirmationDialog={showConfirmDialog}
-          isShowOverflowMenuCol
-          globalNotificationBannerOnClickClose={() => {
-            setShowErrorBanner(false);
-            setShowInviteErrBanner(false);
-            setshowInvitationConflictBanner(false);
-          }}
-          globalNotificationMsgBannerObject={[
-            {
-              autoclose: false,
-              isShow: true,
-              message:
-                "To manage user email name and role, please access SIMS7.",
-              title: "User details are managed in SIMS7",
-              variant: "highlight",
-              hideCloseButton: true
-            },
-            {
-              isShow: !!showErrorBanner,
-              variant: "warning",
-              title: "Information unavailable",
-              message:
-                "A technical issue at our end has stopped us from displaying all information. Please try again later. If the issue persists, please get in touch with our support team.",
-              autoclose: true
-            },
-            {
-              isShow: !!showInvitationConflictBanner,
-              variant: "warning",
-              title: "Invitation conflict",
-              message:
-                "There is an invitation conflict with some users on this list because they are associated with more than one SIMS ID account. Please contact our Service Desk team for assistance in resolving this issue.",
-              autoclose: true
-            },
-            {
-              isShow: !!showInviteErrBanner,
-              variant: "warning",
-              title: "Unable to save",
-              message:
-                "A technical issue at our end has stopped us from saving the changes. Please try again. If the issue persists, please get in touch with our support team. We appreciate your patience and understanding during this time.",
-              autoclose: true
-            }
-          ]}
-          onClickOverflowItem={(e: any, selectedRow: any) => {
-            if (e.target?.innerHTML === "Send Invite") {
-              setShowConfirmDialog(true);
-              setSelectedRowItem(selectedRow);
-            }
-          }}
-          onChangeListCheckBox={(index: number, id: string) => {
-            handleCheckBoxSelection({
-              id,
-              selectedCheckBoxIds,
-              setSelectedCheckBoxIds
-            });
-          }}
-          selectedCheckboxIds={(ids: string[]) => {
-            setSelectedCheckBoxIds(ids);
-          }}
-        />
+            }}
+            selectedCheckboxIds={(ids: string[]) => {
+              setSelectedCheckBoxIds(ids);
+            }}
+          />
+        </div>
       </GridItem>
     </Grid>
   );
