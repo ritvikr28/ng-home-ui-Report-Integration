80d7c91d26dbe3bc2d8d1e4bb45dca3b74fcd7fb
Yashashri Pawar <yashashri.pawar@parentpay>
2025-06-25 14:54:23 +0530

    98986: Added testCases for grid layout

---
+++ src/features/DocumentManagementServer/__tests__/ApiService.spec.tsx
@@ -0,0 +1,128 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { DocumentBasicDetails, SingleDocumentDetail } from '../responseModel';
+import { service } from '../../../shared/utils';
+import { fetchDocumentDetails } from '../ApiService';
+import { AxiosResponse } from 'axios';
+import BreadcrumbWrapper from '../../../shared/components/BreadcrumbWrapper/BreadcrumbWrapper';
+import DocumentManagementServer from '../DocumentManagementServer.logic';
+import { renderHook } from '@testing-library/react-hooks';
+
+const documentResponse: SingleDocumentDetail[] = [
+    {
+        organizationId: "cd0e52dd",
+        userId: "9fbe1bcc",
+        registrationId: 2,
+        fileId: "427e150",
+        personExternalId: "00000000",
+        documentInfo: {
+            fileName: "image",
+            isSelectedForPrepareDownload: true,
+        },
+        document: "image",
+        relatedTo: null,
+        category: "App",
+        addedBy: "SIMS-NG",
+        dateAdded: "11/25/2024 10:04:54",
+        format: "png",
+        size: "2 KB",
+        blobName: "App_427E150E"
+    },
+    {
+        organizationId: "cd0e52ee",
+        userId: "9fbe1bce",
+        registrationId: 3,
+        fileId: "427e151",
+        personExternalId: "00000001",
+        documentInfo: {
+            fileName: "mock image",
+            isSelectedForPrepareDownload: true,
+        },
+        document: "mock image",
+        relatedTo: null,
+        category: "App",
+        addedBy: "SIMS-NG",
+        dateAdded: "11/25/2024 10:04:54",
+        format: "png",
+        size: "20 KB",
+        blobName: "App_427E150E"
+    }
+]
+
+const mockAxiosResponse: AxiosResponse<DocumentBasicDetails> = {
+    data: {
+        pageNumber: 1,
+        pageSize: 20,
+        totalRecords: 2,
+        data: documentResponse
+    },
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config: {}
+};
+
+describe("Fetch document details tests", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    test("should set loading true initially", async () => {
+        jest.spyOn(service, "get").mockResolvedValueOnce(mockAxiosResponse);
+        const { result } = renderHook(() =>
+            DocumentManagementServer({ pageNumber: 1, pageSize: 10 })
+        );
+
+        expect(result.current.loading).toBe(true);
+    });
+
+    test("should return data on successful fetch", async () => {
+        jest.spyOn(service, "get").mockResolvedValueOnce(mockAxiosResponse);
+        const { result, waitForNextUpdate } = renderHook(() =>
+            DocumentManagementServer({ pageNumber: 1, pageSize: 10 })
+        );
+
+        await waitForNextUpdate();
+
+        expect(result.current.loading).toBe(false);
+        expect(result.current.data).toEqual(mockAxiosResponse.data);
+        expect(result.current.error).toBeNull();
+    });
+
+    test("should return GetDocumentDetails data", async () => {
+        jest.spyOn(service, "get").mockResolvedValue(mockAxiosResponse);
+        const response: DocumentBasicDetails | null = await fetchDocumentDetails({ pageNumber: 1, pageSize: 10 });
+        expect(response).toEqual(mockAxiosResponse?.data);
+        expect(service.get).toHaveBeenCalledTimes(1);
+        expect(service.get).toHaveBeenCalled();
+    });
+    test("should return null when API status is not 200", async () => {
+        const mockResponse: Partial<AxiosResponse<DocumentBasicDetails>> = {
+            data: {
+                pageNumber: 1,
+                pageSize: 20,
+                totalRecords: 2,
+                data: documentResponse
+            }, status: 404, statusText: 'Not Found', headers: {}, config: {}
+        };
+        (service.get as jest.Mock).mockResolvedValueOnce(mockResponse);
+
+        const result = await fetchDocumentDetails({ pageNumber: 1, pageSize: 10 });
+        expect(result).toBeNull();
+    });
+
+    test("should return null when an error occurs", async () => {
+        (service.get as jest.Mock).mockRejectedValue(new Error("Network Error"));
+
+        const result = await fetchDocumentDetails({ pageNumber: 1, pageSize: 10 });
+
+        expect(result).toBeNull();
+    });
+
+    test("calls onCloseSideNavigationPanel when Admin Console breadcrumb is clicked", () => {
+        const handleClick = jest.fn();
+        render(<BreadcrumbWrapper />);
+        const adminConsoleBreadcrumb = screen.getByText("breadcrumbshome");
+        fireEvent.click(adminConsoleBreadcrumb);
+        expect(handleClick).toHaveBeenCalledTimes(0);
+    });
+});
