f5ad7c8ff1b18122be0de4e09cc88e22e1e2d837
Yashashri Pawar <yashashri.pawar@parentpay>
2025-07-09 13:14:49 +0530

    132781: Added new changes getDocumentDetails API

--- src/features/DocumentManagementServer/ApiService.tsx
+++ src/features/DocumentManagementServer/ApiService.tsx
@@ -4,18 +4,39 @@ import { service } from "../../shared/utils";
 import { DocumentBasicDetails, DocumentManagementServerProps } from "./responseModel";
 import {PLATFORM_BASEURLS} from "../../ApiConfig.json"
 
-export const fetchDocumentDetails: ({pageNumber, pageSize}: DocumentManagementServerProps) => Promise<DocumentBasicDetails | null> = async ({pageNumber, pageSize}:DocumentManagementServerProps) => {
- 
+export const fetchDocumentDetails = async ({
+  pageNumber,
+  pageSize,
+  searchText = '',
+  categoryId = [],
+  fromDate = '',
+  toDate = '',
+  sortBy = 'DateAdded',
+  sortDirection = 'Desc',
+}: DocumentManagementServerProps): Promise<DocumentBasicDetails | null> => {
   try {
-    const url =`/validation/api/v1/file/getdocumentdetails?DocumentsRequest.PageNumber=${pageNumber}&DocumentsRequest.PageSize=${pageSize}`;
+    const url = `validation/api/v1/file/getdocumentdetails`;
     const baseUrl = buildApplicationUrl(PLATFORM_BASEURLS);
+ 
+    const payload = {
+      documentsRequest: {
+        pageNumber,
+        pageSize,
+        searchText,
+        categoryId,
+        fromDate,
+        toDate,
+        sortBy,
+        sortDirection,
+      },
+    };
+ 
     const responseData: AxiosResponse<DocumentBasicDetails> =
-      await service.get(url, baseUrl);
-      if (responseData?.status === 200) {
+      await service.post(url, payload, { baseURL: baseUrl });
+    if (responseData?.status === 200) {
       return responseData?.data;
     }
     return null;
-
   } catch (err: any) {
     return null;
   }
