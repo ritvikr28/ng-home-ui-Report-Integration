585c10ef6fa08713620cacdf4be46914a2689fc0
jyoti pawar <jyoti.pawar@parentpay.com>
2025-08-28 11:32:53 +0530

    [#141840][Jyoti] If user does have sims connected and and does not exist in authz

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,3 +1,6 @@
+interface HomePageForSimsConnectedNormalUserProps {
+  isRenderSimsConnectedBanner: boolean;
+}
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {
   Suspense,
@@ -47,6 +50,7 @@ import {
   useTranslation,
   UseTranslationResponse
 } from "@essnextgen/ui-intl-kit";
+
 import { hasFeaturePermission } from "@essnextgen/ui-flagr";
 import { saveAppPermission, startRequest } from "./actions/storeActions";
 import { IAppModule } from "./types/AppPermission";
@@ -68,6 +72,7 @@ import InviteUsersLogic from "./pages/InviteUsers";
 import SystemStatus from "./features/SystemStatusAlerts/SystemStatus.view";
 import SIMSConnectedLauncher from "./shared/components/Notification-menu/SIMSConnectedLauncherBanner";
 import { SectionTitle } from "./shared/components/SectionTitle/SectionTitle";
+import { useSimsConnectedBanner } from "./shared/hooks/useSimsConnectedBanner";
 
 const NoAccess: LazyExoticComponent<FC<{}>> = lazy(
   () => import("./pages/NoAccess")
@@ -105,17 +110,13 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
   isStandaloneApp,
   baseRouteName,
 }: ILayoutProps) => {
+  const [isRenderSimsConnectedBanner] = useSimsConnectedBanner();
   const dispatch: any = useDispatch();
   const history: ReturnType<typeof useHistory> = useHistory();
   const { t }: UseTranslationResponse<"translation", undefined> =
     useTranslation();
 
   const [isServiceInitiated, setIsServiceInitiated]: [
-    boolean,
-    React.Dispatch<React.SetStateAction<boolean>>
-  ] = useState<boolean>(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [hasSimsConnected]: [
     boolean,
     React.Dispatch<React.SetStateAction<boolean>>
   ] = useState<boolean>(false);
@@ -249,7 +250,7 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
               isServiceInitiated
                 ? renderHomePage(
                   hasNewHomePagePermission,
-                  hasSimsConnected
+                  isRenderSimsConnectedBanner
                 )
                 : EmptyComponent
             }
@@ -344,14 +345,13 @@ export const Layout: (props: ILayoutProps) => JSX.Element = ({
 /* istanbul ignore next */
 const renderHomePage: (
   hasNewHomePagePermission: boolean,
-  hasSimsConnected: boolean
+  isRenderSimsConnectedBanner: boolean
 ) => React.ComponentType<any> | undefined = (
   hasNewHomePagePermission: boolean,
-  hasSimsConnected: boolean
+  isRenderSimsConnectedBanner: boolean
 ) => {
-
     if (!isAuthzUserAdmin() && !hasNewHomePagePermission) {
-      return () => <HomePageForSimsConnectedNormalUser hasSimsConnected={hasSimsConnected} />;
+      return () => <HomePageForSimsConnectedNormalUser isRenderSimsConnectedBanner={isRenderSimsConnectedBanner} />;
     }
     if (!hasNewHomePagePermission && isAuthzUserAdmin()) {
       return SIMSIDAdminPageView;
@@ -369,7 +369,7 @@ const EmptyComponent: () => JSX.Element = () => (
 );
 
 /* istanbul ignore next */
-const HomePageForSimsConnectedNormalUser: React.FC<{hasSimsConnected: boolean}> = ({hasSimsConnected}) => {
+const HomePageForSimsConnectedNormalUser: React.FC<HomePageForSimsConnectedNormalUserProps> = ({ isRenderSimsConnectedBanner }) => {
   const rel: any = { rel: "noopener noreferrer" };
   const onCardClick: () => void = () => {};
   const { t }: UseTranslationResponse<"translation", undefined> =
@@ -386,12 +386,11 @@ const HomePageForSimsConnectedNormalUser: React.FC<{hasSimsConnected: boolean}> 
   return (
     <div className="home-page-for-sims-connected-normal-user-wrapper">
       <Grid container className="gap-24">
-        {
-          hasSimsConnected &&
+        {isRenderSimsConnectedBanner && (
           <GridItem sm={12} md={12} lg={12} xl={12} xxl={12} className="mt-16">
             <SIMSConnectedLauncher />
           </GridItem>
-        }
+        )}
         <GridItem sm={12} md={12} lg={12} xl={12} xxl={12} className="welcome-heading-container">
           <div>
             <span className="welcome-heading">{t("HomePageForSimsConnectedNormalUser.welcomeToSims")}</span>
--- src/pages/NewHomePage/NewHomePageBanner.view.tsx
+++ src/pages/NewHomePage/NewHomePageBanner.view.tsx
@@ -1,62 +1,39 @@
 /* eslint-disable import/no-duplicates */
-import { useState, useEffect } from "react";
 import React from "react";
 import WhatsNewBanner from "../../shared/components/Notification-menu/ClassViewWhatsNewBanner";
 import SIMSConnectedLauncher from "../../shared/components/Notification-menu/SIMSConnectedLauncherBanner";
-import { fetchLinks } from "../../shared/hooks/useSIMSNextGenLinks";
+import { useSimsConnectedBanner } from "../../shared/hooks/useSimsConnectedBanner";
+import { useEffect } from "react";
 import { BannerProps } from "./NewHomePage.props";
 
 export const NewHomePageBanner: React.FC<BannerProps> = ({
     showClassViewNotification,
     setShowClassViewNotification
 }) => {
-    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [hasConnectedLauncher, isLoading] = useSimsConnectedBanner();
 
-    const [hasConnectedLauncher, sethasConnectedLauncher]: [
-        boolean,
-        React.Dispatch<React.SetStateAction<boolean>>
-    ] = useState<boolean>(false);
-
-    const fetchSimsConnectedLink: () => Promise<void> = async () => {
-        setIsLoading(true);
-        try {
-                    const responseapidata = await fetchLinks();
-                    sethasConnectedLauncher(responseapidata);
-                    setIsLoading(false);
-                    // showClassViewNotification = false;
-                    setShowClassViewNotification(false);
-
-        } catch (error) {
-            setIsLoading(false);
-            console.log(error);
-        }
-    };
-
+    // Restore previous behavior: hide class view notification immediately after loading completes
     useEffect(() => {
-        (async () => {
-            await fetchSimsConnectedLink();
-        })();
-    }, []);
-
-    const renderBanner = () => {
-        if (hasConnectedLauncher && !isLoading) {
-            return (
-                <div data-testid="sims-launcher">
-                    <SIMSConnectedLauncher />
-                </div>
-            );
+        if (!isLoading) {
+            setShowClassViewNotification(false);
         }
-        if (showClassViewNotification && !isLoading) {
-            return (
-                <div data-testid="whats-new-banner">
-                    <WhatsNewBanner />
-                </div>
-            );
-        }
-        return null;
-    };
+    }, [isLoading, setShowClassViewNotification]);
 
-    return renderBanner();
+    if (hasConnectedLauncher && !isLoading) {
+        return (
+            <div data-testid="sims-launcher">
+                <SIMSConnectedLauncher />
+            </div>
+        );
+    }
+    if (showClassViewNotification && !isLoading) {
+        return (
+            <div data-testid="whats-new-banner">
+                <WhatsNewBanner />
+            </div>
+        );
+    }
+    return null;
 };
 
 export default NewHomePageBanner;
