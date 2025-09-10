d512bc843d08068d8115ce1c2d4e4b8081ebbb7f
Viraj Jabare <Viraj.jabre@parentpay.com>
2025-09-10 09:29:47 +0530

    #132349 | Viraj | Test Coverage Fixed

--- src/features/DocumentManagementServer/__tests__/DocumentManagementServer.spec.tsx
+++ src/features/DocumentManagementServer/__tests__/DocumentManagementServer.spec.tsx
@@ -210,6 +210,7 @@ it("shows empty state message on initial load", async () => {
  
 
 it("shows no records on initial load, shows records after search", async () => {
+  jest.setTimeout(15000);
   jest.spyOn(apiService, "fetchDMSSuggestions").mockResolvedValue({
     payload: [
       { name: "Pupil", link: "", values: [
