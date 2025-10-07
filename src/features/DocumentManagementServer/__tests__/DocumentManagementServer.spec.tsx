ece19fffb07f4e9c1571e97106ea91f6d53068e0
Shreyash Khambalkar <shreyash.khambalkar@parentpay.com>
2025-10-07 15:10:48 +0530

    #132252 | Shreyash | test case reduced and time optimized

--- src/features/DocumentManagementServer/__tests__/DocumentManagementServer.spec.tsx
+++ src/features/DocumentManagementServer/__tests__/DocumentManagementServer.spec.tsx
@@ -263,24 +263,6 @@ it("shows suggestions and triggers search when user clicks a suggestion", async 
     ).toBeInTheDocument();
   });
  
-  it("renders download view panel with complete file", async () => {
-    (ApiService.viewDownload as jest.Mock).mockResolvedValue({
-      status: 200,
-      data: [{ name: "File1", status: "complete", fileExpiryDays: 2 }],
-    });
-    render(<MemoryRouter>
-      <DocumentManagementServerView />
-    </MemoryRouter>);
- 
-      fireEvent.click(await screen.findByText("Actions"));
-      fireEvent.click(await screen.findByText("View download"));
- 
-    const sidePanelHeader = await screen.getByText("Downloads");
-    expect(sidePanelHeader).toBeInTheDocument();
-    await waitFor(() => expect(screen.getByText("File1")).toBeInTheDocument());
-    expect(screen.getByText("Expires in 2 day(s).")).toBeInTheDocument();
-  });
- 
   test("removes id from selectedCheckBoxIds and allSelectedDocs when checkbox is unchecked", async () => {
     (ApiService.fetchFilterCategory as jest.Mock).mockResolvedValue([]);
   jest.spyOn(ApiService, "fetchDMSSuggestions").mockResolvedValue(mockSuggestions);
@@ -344,21 +326,6 @@ it("shows suggestions and triggers search when user clicks a suggestion", async 
   fireEvent.click(checkbox);
   expect(checkbox).not.toBeChecked();
 });
-
-  it("renders in-progress downloads", async () => {
-    (ApiService.viewDownload as jest.Mock).mockResolvedValue({
-      status: 200,
-      data: [{ name: "File2", status: "inprogress" }],
-    });
-    render(<MemoryRouter>
-      <DocumentManagementServerView />
-    </MemoryRouter>);
- 
-      fireEvent.click(await screen.findByText("Actions"));
-      fireEvent.click(await screen.findByText("View download"));
- 
-    await waitFor(() => expect(screen.getByText("File2")).toBeInTheDocument());
-  });
  
   it("renders initiated downloads", async () => {
     (ApiService.viewDownload as jest.Mock).mockResolvedValue({
@@ -464,26 +431,6 @@ it("shows suggestions and triggers search when user clicks a suggestion", async 
     await waitFor(() => {
       expect(screen.getByText("FileZero")).toBeInTheDocument();
       expect(screen.getByText("Expires today.")).toBeInTheDocument();
-      expect(screen.getByText("FileUndefined")).toBeInTheDocument();
-    });
-  });
-
-    it("renders side panel files with expiry 0 or undefined", async () => {
-    (ApiService.viewDownload as jest.Mock).mockResolvedValue({
-      status: 200,
-      data: [
-        { name: "FileZero", status: "complete", fileExpiryDays: 1 },
-        { name: "FileUndefined", status: "complete" }
-      ],
-    });
-    render(<MemoryRouter>
-      <DocumentManagementServerView />
-    </MemoryRouter>);
-    fireEvent.click(await screen.findByText("Actions"));
-    fireEvent.click(await screen.findByText("View download"));
-    await waitFor(() => {
-      expect(screen.getByText("FileZero")).toBeInTheDocument();
-      expect(screen.getByText("Expires in 1 day(s).")).toBeInTheDocument();
       expect(screen.getByText("FileUndefined")).toBeInTheDocument();
     });
   });
