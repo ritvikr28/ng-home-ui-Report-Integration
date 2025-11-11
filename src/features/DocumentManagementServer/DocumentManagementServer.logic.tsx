9c0b57486c9299b05027d1110a4345de5bc9a54c
Shreyash Khambalkar <shreyash.khambalkar@parentpay.com>
2025-11-11 13:18:13 +0530

    161426 | Shreyash | removing code as it is fixed in other PR

--- src/features/DocumentManagementServer/DocumentManagementServer.logic.tsx
+++ src/features/DocumentManagementServer/DocumentManagementServer.logic.tsx
@@ -27,15 +27,12 @@ export function renderRelatedToItem(item: any) {
         <a href={href} className="relatedto-link" target="_blank" rel="noopener noreferrer">
           {item.name}
         </a>
-        {(item.year !== null && item.year !== undefined && item.year !== "" &&
-          item.reg !== null && item.reg !== undefined && item.reg !== "") ? (
-          <Tag
-            dataTestId="name"
-            id="name"
-            className="relatedto-tag"
-            text={`${item.year} / ${item.reg}`}
-          />
-        ) : null}
+         <Tag
+          dataTestId="name"
+          id="name"
+          className="relatedto-tag"
+          text={`${item.year}${item.reg ? ` / ${item.reg}` : ""}`}
+        />
       </>
     );
   }
