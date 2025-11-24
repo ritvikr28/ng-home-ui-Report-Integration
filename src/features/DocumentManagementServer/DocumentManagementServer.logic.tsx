8b43dcb1a10d7c661c789e35c09f083395aca545
Shreyash Khambalkar <shreyash.khambalkar@parentpay.com>
2025-11-24 15:51:05 +0530

    #NoCard | Shreyash | delete files changes

--- src/features/DocumentManagementServer/DocumentManagementServer.logic.tsx
+++ src/features/DocumentManagementServer/DocumentManagementServer.logic.tsx
@@ -982,8 +982,25 @@ export const handleBulkDeleteLogic = async ({
         event: "key_action",
         actionType: "delete"
       });
-    if(isHeaderBoxChecked === true){
-    setTimeout(() => {
+    if (isHeaderBoxChecked === true) {
+    let timeoutMs = 0;
+    const count = availableFileIds?.length || 0;
+    if (count <= 100) {
+      timeoutMs = 0;
+    } else if (count > 100 && count <= 200) {
+      timeoutMs = 1000;
+    } else if (count > 200 && count <= 400) {
+      timeoutMs = 2500;
+    } else if (count > 400 && count <= 650) {
+      timeoutMs = 4500;
+    } else if (count > 650 && count <= 1000) {
+      timeoutMs = 7000;
+    }
+    else if (count > 1000) {
+      timeoutMs = 11000;
+    }
+    if (timeoutMs > 0) {
+      setTimeout(() => {
         fetchGetDocumentDetails(currentPage, allRegistrationIds, sortBy, sortDirection);
       }, 3500);
     }
