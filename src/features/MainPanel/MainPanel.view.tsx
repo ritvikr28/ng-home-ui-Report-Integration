ef0839674d0fbf15ada14578ce60511305f2f016
Jyoti Pawar <jyoti.pawar@parentpay.com>
2025-12-04 17:22:59 +0530

    [# 166870][Jyoti] Implement responsiveness layout for homepage with video

--- src/features/MainPanel/MainPanel.view.tsx
+++ src/features/MainPanel/MainPanel.view.tsx
@@ -18,7 +18,7 @@ import { envConfig } from "../../shared/utils";
 import { isOrganisationInVariant } from "../../shared/utils/flagr-utils";
 import { SIMSupdatesView } from "../../shared/components/SIMSUpdates/SIMSupdates.view";
 import { FilledLeftPanelIcon } from "../../shared/components/CommonElement/FilledButton";
-import { HomePageVideoFlagr, homepageVideoOrgView } from "../../Layout";
+import { homepageVideoOrgView } from "../../Layout";
 
 const requiredStaffTimeTablePermissions: Permission[] = [
   {
