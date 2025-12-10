9542441d59fc2b0e2c883e77208413997b36c8d8
Archana Padmanabhi <archana.padmanabhi@parentpay.com>
2025-12-10 07:17:28 +0000

    Merged PR 43813: #168571|Archana|Homepage Videos CORS error
    #168571|Archana|Homepage Videos CORS error
    
    Related work items: #168571

--- src/features/MainPanel/MainPanel.view.tsx
+++ src/features/MainPanel/MainPanel.view.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { MatchPermissions, Permission, authService } from "@essnextgen/auth-ui";
 import {
   Divider,
@@ -21,8 +21,8 @@ import { SIMSupdatesView } from "../../shared/components/SIMSUpdates/SIMSupdates
 import { FilledLeftPanelIcon } from "../../shared/components/CommonElement/FilledButton";
 import { homepageVideoOrgViewExcluded } from "../../Layout";
 import gtmAnalytics from '../../shared/utils/analytics';
-import { saveVideoPlayStatus } from '../../shared/services/videoPlayStatusSave';
-import { fetchVideoPlayStatus } from "../../shared/services/videoPlayStatus";
+// import { saveVideoPlayStatus } from '../../shared/services/videoPlayStatusSave';
+// import { fetchVideoPlayStatus } from "../../shared/services/videoPlayStatus";
 
 const requiredStaffTimeTablePermissions: Permission[] = [
   {
@@ -95,29 +95,29 @@ const MainPanelView: (props: IMainPanelProps) => JSX.Element = (
     setIsOpen(!isOpen);
   };
 
-  const [isPlayed, setIsPlayed] = useState(false);
+  // const [isPlayed, setIsPlayed] = useState(false);
 
-  useEffect(() => {
-    async function loadPlayStatus() {
-      const result = await fetchVideoPlayStatus();
+  // useEffect(() => {
+  //   async function loadPlayStatus() {
+  //     const result = await fetchVideoPlayStatus();
 
-      if (result?.status === 200 && result.payload) {
-        const playedValue =
-          String(result.payload.isPlayed).toLowerCase() === "true";
+  //     if (result?.status === 200 && result.payload) {
+  //       const playedValue =
+  //         String(result.payload.isPlayed).toLowerCase() === "true";
 
-        console.log("Converted isPlayed =", playedValue);
-        setIsPlayed(playedValue);
-      } else {
-        setIsPlayed(false);
-      }
-    }
+  //       console.log("Converted isPlayed =", playedValue);
+  //       setIsPlayed(playedValue);
+  //     } else {
+  //       setIsPlayed(false);
+  //     }
+  //   }
 
-    loadPlayStatus();
-  }, []);
+  //   loadPlayStatus();
+  // }, []);
 
   async function handlePlay() {
     gtmAnalytics.pushEvent({ event: "playVideo" });
-    await saveVideoPlayStatus();
+    // await saveVideoPlayStatus();
     console.log("The video has played.");
   }
 
@@ -131,7 +131,7 @@ const MainPanelView: (props: IMainPanelProps) => JSX.Element = (
     console.log("the video has paused.");
   }
 
-  console.log("isPlayed value:", isPlayed);
+  // console.log("isPlayed value:", isPlayed);
   return (
     <div>
       <Grid className="new-margin-b-container">
@@ -181,7 +181,8 @@ const MainPanelView: (props: IMainPanelProps) => JSX.Element = (
       ) && (
           <>
             <Search isOpen={isOpen} />
-            <div className={!isPlayed ? "wistia-class new-divider-spacing" : "new-divider-spacing"}>
+            <div className="new-divider-spacing">
+              {/* <div className={!isPlayed ? "wistia-class new-divider-spacing" : "new-divider-spacing"}> */}
               <Divider />
             </div>
           </>
@@ -195,13 +196,14 @@ const MainPanelView: (props: IMainPanelProps) => JSX.Element = (
         ) && (
           <>
             <SltViewBett />
-            <div className={!isPlayed ? "wistia-class new-divider-spacing" : "new-divider-spacing"}>
+            {/* <div className={!isPlayed ? "wistia-class new-divider-spacing" : "new-divider-spacing"}> */}
+            <div className="new-divider-spacing">
               <Divider />
             </div>
           </>
         )}
 
-      {(homepageVideoOrgViewExcluded && !isPlayed) ? (
+      {(homepageVideoOrgViewExcluded) ? (
         <div className="wistia-palyer-video-class">
           <WistiaPlayer mediaId="w9mg776ol6"
             onPlay={() => handlePlay()}
