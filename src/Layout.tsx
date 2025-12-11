17296af860984a4182785b23d57dec7933bb318a
Archana Padmanabhi <archana.padmanabhi@parentpay.com>
2025-12-11 19:13:05 +0530

    #166284|Archana|Homepage Videos Feature Flag Implementation

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -52,7 +52,7 @@ import { hasFeaturePermission } from "@essnextgen/ui-flagr";
 import { saveAppPermission, startRequest } from "./actions/storeActions";
 import { IAppModule } from "./types/AppPermission";
 import NewHomepageView from "./pages/NewHomePage/NewHomePage.view";
-import { envConfig, getUserOrganisation, isAuthzUserAdmin, service } from "./shared/utils";
+import { envConfig, isAuthzUserAdmin, service } from "./shared/utils";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import AdminConsole from "./features/AdminConsole/AdminConsole.view";
 import DBManagement from "./features/DBManagement/DBManagement.view";
