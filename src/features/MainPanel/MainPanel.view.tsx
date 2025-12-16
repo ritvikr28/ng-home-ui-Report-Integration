1c1e39c4feaf4a343308b634c1bbb09e8691cb1c
Jyoti Pawar <jyoti.pawar@parentpay.com>
2025-12-16 14:35:50 +0530

    [# 169594][Jyoti] Fire show_video Event on Homepage Initialization

--- src/features/MainPanel/MainPanel.view.tsx
+++ src/features/MainPanel/MainPanel.view.tsx
@@ -111,10 +111,12 @@ const MainPanelView: (props: IMainPanelProps) => JSX.Element = (
   }
 
   function handleOnEnded() {
-    gtmAnalytics.pushVideoEvent(100);
+  gtmAnalytics.pushVideoEvent(100);
+  console.log('The video has ended!');
   };
 
   function handleOnPause() {
+    console.log('the video has paused.');
   }
 
   const shouldShowVideo =
