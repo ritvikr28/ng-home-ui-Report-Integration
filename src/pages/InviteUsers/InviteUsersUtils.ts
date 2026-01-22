b886e66e7f921365055df34021d8ac82e90a9f3d
Adrian Austin <Adrian.Austin@parentpay.com>
2026-01-22 09:46:17 +0000

    Corrected case type-o

--- src/pages/InviteUsers/InviteUsersUtils.ts
+++ src/pages/InviteUsers/InviteUsersUtils.ts
@@ -108,7 +108,7 @@ export const fetchInviteUserDetails = async (props: IPaginationOptions) => {
             : item?.emailId,
         userType: item?.userType,
         invitationStatus: item?.invitationStatus,
-        inviteRequestDate: item?.InviteRequestDate,
+        inviteRequestDate: item?.inviteRequestDate,
         actions: {
           options: [
             {
