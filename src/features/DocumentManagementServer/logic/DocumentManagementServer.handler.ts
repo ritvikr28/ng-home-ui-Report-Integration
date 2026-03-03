23f0b8cdbb9194cf0fbf9556b74a61b5def1ae94
Viraj Jabare <Viraj.jabre@parentpay.com>
2026-03-03 14:17:29 +0530

    #185446 | Hiding Banner

--- src/features/DocumentManagementServer/logic/DocumentManagementServer.handler.ts
+++ src/features/DocumentManagementServer/logic/DocumentManagementServer.handler.ts
@@ -753,7 +753,7 @@ export function getNotificationMsgBannerObject(params: NotificationMsgBannerPara
       autoclose: false
     },
     {
-      isShow: true,
+      isShow: false,
       variant: "warning",
       title: t("DocumentManagementServer.privateFilesBannerTitle"),
       message: getBannerMessageWithLink(t("DocumentManagementServer.privateFilesBannerDescription"), t("DocumentManagementServer.learnMoreKnowledgeBase")) ,
